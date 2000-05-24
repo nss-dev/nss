ea7db8ffb34614902bb55e6b2a4482292ba4a634
nelsonb%netscape.com <none@none>
2000-05-24 03:35:23 +0000

    Fix the logic in client and server to detect version roll-back attack, rolling back from TLS (SSL 3.1) to SSL 3.0.  Provide a new SSL socket option to disable roll-back detection in servers, since certain TLS clients are doing it incorrectly.

--- security/nss/lib/ssl/sslcon.c
+++ security/nss/lib/ssl/sslcon.c
@@ -3075,16 +3075,15 @@ invalid:
     cp = msg = ci->sendBuf.buf;
     msg[0] = SSL_MT_CLIENT_HELLO;
     if ( ss->enableTLS ) {
-	msg[1] = MSB(SSL_LIBRARY_VERSION_3_1_TLS);
-	msg[2] = LSB(SSL_LIBRARY_VERSION_3_1_TLS);
+	ss->clientHelloVersion = SSL_LIBRARY_VERSION_3_1_TLS;
     } else if ( ss->enableSSL3 ) {
-	msg[1] = MSB(SSL_LIBRARY_VERSION_3_0);
-	msg[2] = LSB(SSL_LIBRARY_VERSION_3_0);
+	ss->clientHelloVersion = SSL_LIBRARY_VERSION_3_0;
     } else {
-	msg[1] = MSB(SSL_LIBRARY_VERSION_2);
-	msg[2] = LSB(SSL_LIBRARY_VERSION_2);
+	ss->clientHelloVersion = SSL_LIBRARY_VERSION_2;
     }
     
+    msg[1] = MSB(ss->clientHelloVersion);
+    msg[2] = LSB(ss->clientHelloVersion);
     msg[3] = MSB(localCipherSize);
     msg[4] = LSB(localCipherSize);
     msg[5] = MSB(sidLen);
