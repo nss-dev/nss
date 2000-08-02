0709851495aa9c59c3b1b9c0632933448ed3df77
nelsonb%netscape.com <none@none>
2000-08-02 20:47:05 +0000

    Declare s_mp_mul_2d(), which is complementary to s_mp_div_2d().

--- security/nss/lib/freebl/mpi/mpi-priv.h
+++ security/nss/lib/freebl/mpi/mpi-priv.h
@@ -174,6 +174,7 @@ void     s_mp_exch(mp_int *a, mp_int *b);      /* swap a and b in place   */
 
 mp_err   s_mp_lshd(mp_int *mp, mp_size p);     /* left-shift by p digits  */
 void     s_mp_rshd(mp_int *mp, mp_size p);     /* right-shift by p digits */
+mp_err   s_mp_mul_2d(mp_int *mp, mp_digit d);  /* multiply by 2^d in place */
 void     s_mp_div_2d(mp_int *mp, mp_digit d);  /* divide by 2^d in place  */
 void     s_mp_mod_2d(mp_int *mp, mp_digit d);  /* modulo 2^d in place     */
 void     s_mp_div_2(mp_int *mp);               /* divide by 2 in place    */
