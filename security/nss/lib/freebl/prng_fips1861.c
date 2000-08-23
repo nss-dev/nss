4cc83dba1a50480a4e42c387ba483a7d8fde081f
mcgreer%netscape.com <>
2000-08-23 18:11:47 +0000

    don't zero input to random update (RNG_SystemInfoForRNG uses environment as input).  Fix for bug 49898.

--- security/nss/lib/freebl/prng_fips1861.c
+++ security/nss/lib/freebl/prng_fips1861.c
@@ -334,7 +334,6 @@ prng_RandomUpdate(RNGContext *rng, void *data, size_t bytes, unsigned char *q)
     /* --- UNLOCKED --- */
     /* housekeeping */
     memset(inputhash, 0, BSIZE);
-    memset(data, 0, bytes);  /* let the caller free */
     return rv;
 }
 
