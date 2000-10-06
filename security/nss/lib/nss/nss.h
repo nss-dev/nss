189f7c53eedef7799da0a3dfd2bd6e3a5aa14c8f
nelsonb%netscape.com <>
2000-10-06 21:37:25 +0000

    Implement new function NSS_InitReadWrite, which is just like NSS_Init except that it opens the files read/write.  This is to replace the broken SECU_ routines in nss/cmd/lib.

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -42,12 +42,20 @@
 
 SEC_BEGIN_PROTOS
 /*
- * Open the Cert, Key, and Security Module databases.
+ * Open the Cert, Key, and Security Module databases, read only.
  * Initialize the Random Number Generator.
  * Does not initialize the cipher policies or enables.
  * Default policy settings disallow all ciphers.
  */
 extern SECStatus NSS_Init(const char *configdir);
+
+/*
+ * Open the Cert, Key, and Security Module databases, read/write.
+ * Initialize the Random Number Generator.
+ * Does not initialize the cipher policies or enables.
+ * Default policy settings disallow all ciphers.
+ */
+extern SECStatus NSS_InitReadWrite(const char *configdir);
 
 /*
  * initialize NSS without a creating cert db's, key db's, or secmod db's.
