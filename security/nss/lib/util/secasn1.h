6af4c1e0ff7bc6cc9ee237eb78e55d24936b98f9
wtc%netscape.com <none@none>
2001-05-02 20:14:46 +0000

    Bugzilla bug #78528: put the const ASN1 template declarations inside an extern "C" block.  Thanks to Javier Delgadillo for the patch.

--- security/nss/lib/util/secasn1.h
+++ security/nss/lib/util/secasn1.h
@@ -169,7 +169,6 @@ extern const SEC_ASN1Template *
 SEC_ASN1GetSubtemplate (const SEC_ASN1Template *inTemplate, void *thing,
 			PRBool encoding);
 
-SEC_END_PROTOS
 /************************************************************************/
 
 /*
@@ -278,4 +277,5 @@ SEC_ASN1_CHOOSER_DECLARE(SEC_PointerToOctetStringTemplate)
 
 SEC_ASN1_CHOOSER_DECLARE(SEC_SetOfAnyTemplate)
 
+SEC_END_PROTOS
 #endif /* _SECASN1_H_ */
