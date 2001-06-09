14c434fff6c7e65d59e9cf60161a5cab8ae6abd5
nelsonb%netscape.com <>
2001-06-09 03:20:13 +0000

    Change ssl_Time() to use time() instead of PR_Now on systems that have it.

--- security/nss/lib/ssl/sslnonce.c
+++ security/nss/lib/ssl/sslnonce.c
@@ -44,7 +44,9 @@
 #include "sslproto.h"
 #include "nssilock.h"
 #include "nsslocks.h"
-
+#if defined(XP_UNIX) || defined(XP_WIN) || defined(_WINDOWS)
+#include <time.h>
+#endif
 
 PRUint32 ssl_sid_timeout = 100;
 PRUint32 ssl3_sid_timeout = 86400L; /* 24 hours */
@@ -337,14 +339,19 @@ SSL_ClearSessionCache(void)
 PRUint32
 ssl_Time(void)
 {
+    PRUint32 myTime;
+#if defined(XP_UNIX) || defined(XP_WIN) || defined(_WINDOWS)
+    myTime = time(NULL);	/* accurate until the year 2038. */
+#else
+    /* portable, but possibly slower */
     PRTime now;
     PRInt64 ll;
-    PRUint32 time;
 
     now = PR_Now();
     LL_I2L(ll, 1000000L);
     LL_DIV(now, now, ll);
-    LL_L2UI(time, now);
-    return time;
+    LL_L2UI(myTime, now);
+#endif
+    return myTime;
 }
 
