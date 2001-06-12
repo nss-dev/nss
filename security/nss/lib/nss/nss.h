78bcd2b59d2cfda14bc09dd50e088ea750329eea
nicolson%netscape.com <none@none>
2001-06-12 20:57:20 +0000

    Changes for NSS/JSS integration.

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -131,6 +131,13 @@ SECStatus NSS_NoDB_Init(const char *configdir);
  */
 extern void NSS_Shutdown(void);
 
+/*
+ * Returns PR_TRUE if NSS has already been successfully initialized,
+ * PR_FALSE otherwise.
+ */
+PRBool NSS_IsInitialized();
+
+
 SEC_END_PROTOS
 
 #endif /* __nss_h_ */
