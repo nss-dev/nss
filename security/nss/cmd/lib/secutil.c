76eb7b40dc7ea812b4be5b35fa700bf8bb8f00b1
mcgreer%netscape.com <none@none>
2001-09-17 19:17:01 +0000

    merge tip fixes
    --HG--
    branch : NSS_3_3_BRANCH

--- security/nss/cmd/lib/secutil.c
+++ security/nss/cmd/lib/secutil.c
@@ -214,10 +214,11 @@ char *
 SECU_GetModulePassword(PK11SlotInfo *slot, PRBool retry, void *arg) 
 {
     char prompt[255];
-    secuPWData *pwdata = arg;
+    secuPWData *pwdata = (secuPWData *)arg;
     secuPWData pwnull = { PW_NONE, 0 };
+    char *pw;
 
-    if (arg == NULL)
+    if (pwdata == NULL)
 	pwdata = &pwnull;
 
     if (retry && pwdata->source != PW_NONE) {
@@ -225,20 +226,27 @@ SECU_GetModulePassword(PK11SlotInfo *slot, PRBool retry, void *arg)
     	return NULL;
     }
 
-    sprintf(prompt, "Enter Password or Pin for \"%s\":",
-	    PK11_GetTokenName(slot));
-
     switch (pwdata->source) {
     case PW_NONE:
+	sprintf(prompt, "Enter Password or Pin for \"%s\":",
+	                 PK11_GetTokenName(slot));
 	return SECU_GetPasswordString(NULL, prompt);
     case PW_FROMFILE:
-	return SECU_FilePasswd(slot, retry, pwdata->data);
+	/* Instead of opening and closing the file every time, get the pw
+	 * once, then keep it in memory (duh).
+	 */
+	pw = SECU_FilePasswd(slot, retry, pwdata->data);
+	pwdata->source = PW_PLAINTEXT;
+	pwdata->data = PL_strdup(pw);
+	/* it's already been dup'ed */
+	return pw;
     case PW_PLAINTEXT:
-	return PL_strdup(arg);
+	return PL_strdup(pwdata->data);
     default:
 	break;
     }
 
+    PR_fprintf(PR_STDERR, "Password check failed:  No password found.\n");
     return NULL;
 }
 
