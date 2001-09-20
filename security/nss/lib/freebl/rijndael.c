6c6e48f9bfec42cae0afaa70e881be80016126bd
ian.mcgreer%sun.com <>
2001-09-20 17:11:08 +0000

    disable AES for blocksizes > 128 until it is fixed.

--- security/nss/lib/freebl/rijndael.c
+++ security/nss/lib/freebl/rijndael.c
@@ -441,6 +441,8 @@ rijndael_encryptBlock(AESContext *cx,
                       unsigned char *output,
                       const unsigned char *input)
 {
+    return SECFailure;
+#ifdef rijndael_large_blocks_fixed
     unsigned int j, r, Nb;
     unsigned int c2, c3;
     PRUint32 *roundkeyw;
@@ -473,6 +475,7 @@ rijndael_encryptBlock(AESContext *cx,
 	                     *roundkeyw++;
     }
     return SECSuccess;
+#endif
 }
 
 SECStatus 
@@ -480,6 +483,8 @@ rijndael_decryptBlock(AESContext *cx,
                       unsigned char *output,
                       const unsigned char *input)
 {
+    return SECFailure;
+#ifdef rijndael_large_blocks_fixed
     int j, r, Nb;
     int c2, c3;
     PRUint32 *roundkeyw;
@@ -513,6 +518,7 @@ rijndael_decryptBlock(AESContext *cx,
 	COLUMN(output, j) ^= *roundkeyw--;
     }
     return SECSuccess;
+#endif
 }
 
 /**************************************************************************
