f82faabe4c3b5404a1c59de4ef08ab55bb5d8591
ian.mcgreer%sun.com <>
2001-10-08 20:19:30 +0000

    make this module buildable using NSS 3.x headers; some cleanup

--- security/nss/lib/dev/devm.h
+++ security/nss/lib/dev/devm.h
@@ -38,13 +38,13 @@
 static const char DEVM_CVS_ID[] = "@(#) $RCSfile$ $Revision$ $Date$ $Name$";
 #endif /* DEBUG */
 
+#ifdef NSS_3_4_CODE
+#include "pkcs11t.h"
+#else
 #ifndef NSSCKT_H
 #include "nssckt.h"
 #endif /* NSSCKT_H */
-
-#ifndef DEVM_H
-#include "devm.h"
-#endif /* DEVM_H */
+#endif /* NSS_3_4_CODE */
 
 #ifndef BASE_H
 #include "base.h"
