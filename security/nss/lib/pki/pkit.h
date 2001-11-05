b3a5ce642631e2ec20aec7a7b23110b089e0b781
ian.mcgreer%sun.com <none@none>
2001-11-05 17:29:27 +0000

    code for handling trust through softoken

--- security/nss/lib/pki/pkit.h
+++ security/nss/lib/pki/pkit.h
@@ -83,6 +83,7 @@ typedef struct nssDecodedCertStr nssDecodedCert;
 struct NSSTrustStr 
 {
     CK_TRUST serverAuth;
+    CK_TRUST clientAuth;
     CK_TRUST emailProtection;
     CK_TRUST codeSigning;
 };
