199472fb46cbc7c09e347dd5bf945377f3ee546e
ian.mcgreer%sun.com <>
2001-11-16 19:36:43 +0000

    fix unneeded lock release in list iteration

--- security/nss/lib/base/list.c
+++ security/nss/lib/base/list.c
@@ -320,7 +320,6 @@ nssListIterator_Start(nssListIterator *iter)
 {
     NSSLIST_LOCK_IF(iter->list);
     if (iter->list->count == 0) {
-	NSSLIST_UNLOCK_IF(iter->list);
 	return NULL;
     }
     iter->current = iter->list->head;
