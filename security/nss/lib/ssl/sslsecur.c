c333ed08f8342dfd7bd36d5a753348227b020bcc
nelsonb%netscape.com <none@none>
2002-03-23 01:06:08 +0000

    If an error occurs when attempting to write previously buffered data on a socket, do not override the error code with PR_WOULD_BLOCK_ERROR. bug 132899.

--- security/nss/lib/ssl/sslsecur.c
+++ security/nss/lib/ssl/sslsecur.c
@@ -1016,7 +1016,7 @@ ssl_SecureSend(sslSocket *ss, const unsigned char *buf, int len, int flags)
     if (ss->pendingBuf.len != 0) {
 	PORT_Assert(ss->pendingBuf.len > 0);
 	rv = ssl_SendSavedWriteData(ss, &ss->pendingBuf, ssl_DefSend);
-	if (ss->pendingBuf.len != 0) {
+	if (rv >= 0 && ss->pendingBuf.len != 0) {
 	    PORT_Assert(ss->pendingBuf.len > 0);
 	    PORT_SetError(PR_WOULD_BLOCK_ERROR);
 	    rv = SECFailure;
