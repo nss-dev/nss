c75e2303b01b1ca4d35436fef772d9abaa2c497d
nelsonb%netscape.com <none@none>
2002-09-23 21:32:35 +0000

    Change coding style used in nss/lib/pki to one that works with ctags. Bug 169999.   Modified Files:   asymmkey.c certdecode.c certificate.c cryptocontext.c pki3hack.c   pkibase.c pkistore.c symmkey.c tdcache.c trustdomain.c

--- security/nss/lib/pki/certdecode.c
+++ security/nss/lib/pki/certdecode.c
@@ -46,22 +46,19 @@ static const char CVS_ID[] = "@(#) $RCSfile$ $Revision$ $Date$ $Name$";
 #ifdef NSS_3_4_CODE
 /* This is defined in nss3hack.c */
 NSS_EXTERN nssDecodedCert *
-nssDecodedPKIXCertificate_Create
-(
+nssDecodedPKIXCertificate_Create (
   NSSArena *arenaOpt,
   NSSDER *encoding
 );
 
 NSS_IMPLEMENT PRStatus
-nssDecodedPKIXCertificate_Destroy
-(
+nssDecodedPKIXCertificate_Destroy (
   nssDecodedCert *dc
 );
 #else /* NSS_4_0_CODE */
 /* This is where 4.0 PKIX code will handle the decoding */
 static nssDecodedCert *
-nssDecodedPKIXCertificate_Create
-(
+nssDecodedPKIXCertificate_Create (
   NSSArena *arenaOpt,
   NSSDER *encoding
 )
@@ -70,8 +67,7 @@ nssDecodedPKIXCertificate_Create
 }
 
 static PRStatus
-nssDecodedPKIXCertificate_Destroy
-(
+nssDecodedPKIXCertificate_Destroy (
   nssDecodedCert *dc
 )
 {
@@ -80,8 +76,7 @@ nssDecodedPKIXCertificate_Destroy
 #endif /* not NSS_3_4_CODE */
 
 NSS_IMPLEMENT nssDecodedCert *
-nssDecodedCert_Create
-(
+nssDecodedCert_Create (
   NSSArena *arenaOpt,
   NSSDER *encoding,
   NSSCertificateType type
@@ -102,8 +97,7 @@ nssDecodedCert_Create
 }
 
 NSS_IMPLEMENT PRStatus
-nssDecodedCert_Destroy
-(
+nssDecodedCert_Destroy (
   nssDecodedCert *dc
 )
 {
