d3f3147e4eef64eaa515e9ff44c95ae8168837fe
relyea%netscape.com <none@none>
2003-02-05 00:29:35 +0000

    1) turn on mangle builds. 2) better fix for the missing MAXPATHLEN missing define. 3) make room for the '\0' in the pathname.

--- security/nss/cmd/shlibsign/shlibsign.c
+++ security/nss/cmd/shlibsign/shlibsign.c
@@ -52,16 +52,14 @@
 #include "pk11sdr.h"
 #include "secrng.h"
 #include "shsign.h"
+#include "pk11pqg.h"
 
 #ifdef USES_LINKS
 #include "libgen.h"
 #include "unistd.h"
+#include "sys/param.h"
 #include "sys/types.h"
 #include "sys/stat.h"
-
-#ifndef MAXPATHLEN
-#define MAXPATHLEN 1024
-#endif
 #endif
 
 static void
@@ -159,7 +157,7 @@ main (int argc, char **argv)
 #ifdef USES_LINKS
     int ret;
     struct stat stat_buf;
-    char link_buf[MAXPATHLEN];
+    char link_buf[MAXPATHLEN+1];
     char *link_file = NULL;
 #endif
 
@@ -262,7 +260,7 @@ main (int argc, char **argv)
     }
     if (S_ISLNK(stat_buf.st_mode)) {
 	char *path,*dirpath;
-	ret = readlink(input_file, link_buf, sizeof(link_buf));
+	ret = readlink(input_file, link_buf, sizeof(link_buf) - 1);
 	if (ret < 0) {
 	   perror(input_file);
 	   goto loser;
