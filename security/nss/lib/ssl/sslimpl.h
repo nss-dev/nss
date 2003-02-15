619df9da51afd11567343c0bea7eb7f1804afaf9
relyea%netscape.com <none@none>
2003-02-15 01:21:25 +0000

    Bug 167756. Address Nelson's review comments. remove socket specific latency in favor of a slot specific latency test (already done by pk11wrap code).

--- security/nss/lib/ssl/sslimpl.h
+++ security/nss/lib/ssl/sslimpl.h
@@ -847,10 +847,6 @@ struct sslSecurityInfoStr {
 
     /* These are used during a connection handshake */
     sslConnectInfo   ci;					/* ssl 2 & 3 */
-    PRIntervalTime   lastTime;			/* last time smart card 
-						 * checked */ /* ssl3 only */  
-    PRBool           lastState;	                /* last state of client auth 
-						 * smart card present (ssl 3) */
 
 };
 
