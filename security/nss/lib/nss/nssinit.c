366e2587feb05d7ef0c4256789d9f192fbeb4213
ian.mcgreer%sun.com <none@none>
2003-02-28 21:13:20 +0000

    bug 191757, InitOIDHash() not threadsafe r=relyea

--- security/nss/lib/nss/nssinit.c
+++ security/nss/lib/nss/nssinit.c
@@ -397,6 +397,8 @@ nss_FindExternalRoot(const char *dbpath, const char* secmodprefix)
 
 static PRBool nss_IsInitted = PR_FALSE;
 
+extern SECStatus secoid_Init(void);
+
 static SECStatus
 nss_Init(const char *configdir, const char *certPrefix, const char *keyPrefix,
 		 const char *secmodName, PRBool readOnly, PRBool noCertDB, 
@@ -466,6 +468,9 @@ loser:
     }
 
     if (rv == SECSuccess) {
+	if (secoid_Init() != SECSuccess) {
+	    return SECFailure;
+	}
 	if (STAN_LoadDefaultNSS3TrustDomain() != PR_SUCCESS) {
 	    return SECFailure;
 	}
