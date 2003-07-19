6b93c411685b5bb12103f26ed9d677121cc6281c
wtc%netscape.com <none@none>
2003-07-19 21:54:54 +0000

    Set NSS version to 3.7.8. Tag: NSS_3_7_BRANCH
    --HG--
    branch : NSS_3_7_BRANCH

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -49,11 +49,11 @@ SEC_BEGIN_PROTOS
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>] [<Beta>]"
  */
-#define NSS_VERSION  "3.7.8 Beta"
+#define NSS_VERSION  "3.7.8"
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   7
 #define NSS_VPATCH   8
-#define NSS_BETA     PR_TRUE
+#define NSS_BETA     PR_FALSE
 
 
 /*
