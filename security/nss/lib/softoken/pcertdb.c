1cc6bbbf2a9a804435c64f129ef8031693c0c1c4
bishakhabanerjee%netscape.com <>
2003-07-28 22:55:16 +0000

    Bug 213903: removing unused variables 'delold', 'save', 'entry'

--- security/nss/lib/softoken/pcertdb.c
+++ security/nss/lib/softoken/pcertdb.c
@@ -2662,7 +2662,6 @@ static SECStatus
 nsslowcert_UpdateSubjectEmailAddr(NSSLOWCERTCertDBHandle *dbhandle, 
 	SECItem *derSubject, char *emailAddr, nsslowcertUpdateType updateType)
 {
-    PRBool save = PR_FALSE, delold = PR_FALSE;
     certDBEntrySubject *entry = NULL;
     int index = -1, i;
     SECStatus rv;
@@ -5235,7 +5234,6 @@ SECStatus
 nsslowcert_SaveSMimeProfile(NSSLOWCERTCertDBHandle *dbhandle, char *emailAddr, 
 	SECItem *derSubject, SECItem *emailProfile, SECItem *profileTime)
 {
-    certDBEntrySMime *entry = NULL;
     SECStatus rv = SECFailure;;
 
     rv = db_BeginTransaction(dbhandle->permCertDB);
