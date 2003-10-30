87cc8ec9663f84c294c49b403bf075c13e5f6ff7
ian.mcgreer%sun.com <none@none>
2003-10-30 18:01:47 +0000

    set to rtm
    --HG--
    branch : SUN_SECURITY_3_3_BRANCH

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -49,11 +49,11 @@ SEC_BEGIN_PROTOS
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>] [<Beta>]"
  */
-#define NSS_VERSION  "3.3.7beta"
+#define NSS_VERSION  "3.3.7rtm"
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   3
 #define NSS_VPATCH   7
-#define NSS_BETA     PR_TRUE
+#define NSS_BETA     PR_FALSE
 
 
 /*
