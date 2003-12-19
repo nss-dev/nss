a4753a1a00a98ce4dce7192e483e18b854cb83e7
wchang0222%aol.com <>
2003-12-19 22:08:12 +0000

    Bugscape bug 54627: made the fix for NSS_CMSSignedData_Encode_BeforeData the same as the code in NSS_CMSSignedData_Decode_BeforeData.  r=nelsonb.

--- security/nss/lib/smime/cmssigdata.c
+++ security/nss/lib/smime/cmssigdata.c
@@ -213,10 +213,8 @@ NSS_CMSSignedData_Encode_BeforeData(NSSCMSSignedData *sigd)
         return SECFailure;
     }
     /* set up the digests */
-    if (sigd->digests && sigd->digests[0]) {
-	sigd->contentInfo.digcx = NULL; /* don't attempt to make new ones. */
-    } else 
-    if (sigd->digestAlgorithms != NULL) {
+    if (sigd->digestAlgorithms != NULL && sigd->digests == NULL) {
+	/* if digests are already there, do nothing */
 	sigd->contentInfo.digcx = 
 	        NSS_CMSDigestContext_StartMultiple(sigd->digestAlgorithms);
 	if (sigd->contentInfo.digcx == NULL)
