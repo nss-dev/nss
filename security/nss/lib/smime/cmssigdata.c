5b7555d381f6c808e64c69c5715febec52b80ade
wchang0222%aol.com <>
2003-12-19 22:54:20 +0000

    Backed out the previous checkin, which broke our S/MIME QA tests.

--- security/nss/lib/smime/cmssigdata.c
+++ security/nss/lib/smime/cmssigdata.c
@@ -213,8 +213,9 @@ NSS_CMSSignedData_Encode_BeforeData(NSSCMSSignedData *sigd)
         return SECFailure;
     }
     /* set up the digests */
-    if (sigd->digestAlgorithms != NULL && sigd->digests == NULL) {
-	/* if digests are already there, do nothing */
+    if (sigd->digests && sigd->digests[0]) {
+	sigd->contentInfo.digcx = NULL; /* don't attempt to make new ones. */
+    } else if (sigd->digestAlgorithms != NULL) {
 	sigd->contentInfo.digcx = 
 	        NSS_CMSDigestContext_StartMultiple(sigd->digestAlgorithms);
 	if (sigd->contentInfo.digcx == NULL)
