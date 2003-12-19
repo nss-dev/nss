7075a5d26c19434ce9e32db1a3a1194cea612d13
nelsonb%netscape.com <>
2003-12-19 03:58:28 +0000

    Don't overwrite pointers to existing message digests if they've been precomputed.  Bugscape bug 54627.  r=wtc, jpierre.

--- security/nss/lib/smime/cmssigdata.c
+++ security/nss/lib/smime/cmssigdata.c
@@ -213,8 +213,12 @@ NSS_CMSSignedData_Encode_BeforeData(NSSCMSSignedData *sigd)
         return SECFailure;
     }
     /* set up the digests */
+    if (sigd->digests && sigd->digests[0]) {
+	sigd->contentInfo.digcx = NULL; /* don't attempt to make new ones. */
+    } else 
     if (sigd->digestAlgorithms != NULL) {
-	sigd->contentInfo.digcx = NSS_CMSDigestContext_StartMultiple(sigd->digestAlgorithms);
+	sigd->contentInfo.digcx = 
+	        NSS_CMSDigestContext_StartMultiple(sigd->digestAlgorithms);
 	if (sigd->contentInfo.digcx == NULL)
 	    return SECFailure;
     }
