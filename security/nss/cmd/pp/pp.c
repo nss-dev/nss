a296f23dbb32bad6ddfd18a612c460bd1a5af650
nelsonb%netscape.com <none@none>
2004-01-29 22:48:58 +0000

    Invoke SECU_RegisterDynamicOids() so that more OIDs will print out. Bug 132942.  a=wtc.

--- security/nss/cmd/pp/pp.c
+++ security/nss/cmd/pp/pp.c
@@ -132,6 +132,7 @@ int main(int argc, char **argv)
 	fprintf(stderr, "%s: NSS_NoDB_Init failed\n", progName);
 	exit(1);
     }
+    SECU_RegisterDynamicOids();
 
     rv = SECU_ReadDERFromFile(&der, inFile, ascii);
     if (rv != SECSuccess) {
