a1499e73497d95c241e68d0e3d1bdf245804fa8a
cvs2hg <none@none>
2004-02-11 02:33:52 +0000

    fixup commit for tag 'MOZILLA_1_7a_RELEASE'

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -49,10 +49,10 @@ SEC_BEGIN_PROTOS
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>] [<Beta>]"
  */
-#define NSS_VERSION  "3.10 Beta"
+#define NSS_VERSION  "3.9.1 Beta"
 #define NSS_VMAJOR   3
-#define NSS_VMINOR   10
-#define NSS_VPATCH   0
+#define NSS_VMINOR   9
+#define NSS_VPATCH   1
 #define NSS_BETA     PR_TRUE
 
 
