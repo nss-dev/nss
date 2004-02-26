537b3e284718b36aa443666b30f482ab44bf1b33
nelsonb%netscape.com <none@none>
2004-02-26 00:05:29 +0000

    Detect NULL arguments to CERT_DecodeTrustString instead of crashing. Bug 235617. r=wtc.

--- security/nss/lib/certdb/certdb.c
+++ security/nss/lib/certdb/certdb.c
@@ -2014,9 +2014,17 @@ CERT_DecodeTrustString(CERTCertTrust *trust, char *trusts)
     unsigned int i;
     unsigned int *pflags;
     
+    if (!trust) {
+	PORT_SetError(SEC_ERROR_INVALID_ARGS);
+	return SECFailure;
+    }
     trust->sslFlags = 0;
     trust->emailFlags = 0;
     trust->objectSigningFlags = 0;
+    if (!trusts) {
+	PORT_SetError(SEC_ERROR_INVALID_ARGS);
+	return SECFailure;
+    }
 
     pflags = &trust->sslFlags;
     
