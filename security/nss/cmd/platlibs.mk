4f9e869e72391d334a6d4dac40c8a3b6cc899e46
saul.edwards%sun.com <none@none>
2004-03-16 07:30:42 +0000

    Add RPATHs for tools: /usr/lib/mps/secv1 - Sol, /opt/sun/private/lib -  Linux
    --HG--
    branch : SUN_SECURITY_3_3_BRANCH

--- security/nss/cmd/platlibs.mk
+++ security/nss/cmd/platlibs.mk
@@ -112,6 +112,15 @@ EXTRA_LIBS += \
 
 ifeq ($(OS_ARCH), SunOS) 
 EXTRA_SHARED_LIBS += -R '$$ORIGIN/../lib' -R '$$ORIGIN/..'
+ifeq ($(USE_64), 1)
+EXTRA_SHARED_LIBS += -R '/usr/lib/mps/secv1/sparcv9'
+else
+EXTRA_SHARED_LIBS += -R '/usr/lib/mps/secv1'
+endif
+endif
+
+ifeq ($(OS_ARCH), Linux)
+EXTRA_SHARED_LIBS += -Wl,-rpath,/opt/sun/private/lib
 endif
 
 ifeq ($(OS_ARCH), AIX) 
