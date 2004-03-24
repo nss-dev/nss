d2c3884ee642e7f5d8610605bc50c39b5f0bf138
glen.beasley%sun.com <none@none>
2004-03-24 11:49:18 +0000

    Switch to NSS_VERSION 3.3.4.4
    --HG--
    branch : NSS_3_3_4_1_BRANCH

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -49,7 +49,7 @@ SEC_BEGIN_PROTOS
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>] [<Beta>]"
  */
-#define NSS_VERSION  "3.3.4.3"
+#define NSS_VERSION  "3.3.4.4"
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   3
 #define NSS_VPATCH   4
