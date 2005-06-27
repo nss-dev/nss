9a7ad2279c74dc267a8fb3d2e43bf0a35a73d6d2
christophe.ravel.bugs%sun.com <none@none>
2005-06-27 18:21:02 +0000

    After branching NSS_3_10_BRANCH, the tip target is 3.11

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -52,10 +52,10 @@ SEC_BEGIN_PROTOS
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>] [<Beta>]"
  */
-#define NSS_VERSION  "3.10.1 Beta"
+#define NSS_VERSION  "3.11 Beta"
 #define NSS_VMAJOR   3
-#define NSS_VMINOR   10
-#define NSS_VPATCH   1
+#define NSS_VMINOR   11
+#define NSS_VPATCH   0
 #define NSS_BETA     PR_TRUE
 
 
