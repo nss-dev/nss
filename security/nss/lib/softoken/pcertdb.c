8b3cc209be3d33103d044d7e3b34b42c0739a640
relyea%netscape.com <>
2005-08-12 22:19:19 +0000

    Correct mistyped version of wtc patch.

--- security/nss/lib/softoken/pcertdb.c
+++ security/nss/lib/softoken/pcertdb.c
@@ -4082,8 +4082,7 @@ openNewCertDB(const char *appName, const char *prefix, const char *certdbname,
     if (appName &&
        (updatedb = dbsopen(certdbname, NO_RDONLY, 0600, DB_HASH, 0)) != NULL) {
 	rv = UpdateV8DB(handle, updatedb);
-    }
-    if ((updatedb = nsslowcert_openolddb(namecb,cbarg,7)) != NULL) {
+    } else if ((updatedb = nsslowcert_openolddb(namecb,cbarg,7)) != NULL) {
 	rv = UpdateV7DB(handle, updatedb);
     } else if ((updatedb = nsslowcert_openolddb(namecb,cbarg,6)) != NULL) {
 	rv = UpdateV6DB(handle, updatedb);
