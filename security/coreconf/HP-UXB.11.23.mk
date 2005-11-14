8961ab5c7dbd46776d81721f715a48748cc13f0a
wtchang%redhat.com <none@none>
2005-11-14 23:49:44 +0000

    Bugzilla Bug 312202: checked in HP-UX Itanium changes contributed by Grace Lu of HP.  r=wtc. Modified Files: HP-UX.mk Added Files: HP-UXB.11.23.mk  Tag: JSS_3_X_BRANCH
    --HG--
    branch : JSS_3_X_BRANCH

--- security/coreconf/HP-UXB.11.23.mk
+++ security/coreconf/HP-UXB.11.23.mk
@@ -16,7 +16,7 @@
 #
 # The Initial Developer of the Original Code is
 # Netscape Communications Corporation.
-# Portions created by the Initial Developer are Copyright (C) 1994-2000
+# Portions created by the Initial Developer are Copyright (C) 2002
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s):
@@ -35,59 +35,25 @@
 #
 # ***** END LICENSE BLOCK *****
 
-#
-# Config stuff for HP-UX
-#
+# On HP-UX 10.30 and 11.x, the default implementation strategy is
+# pthreads.  Classic nspr and pthreads-user are also available.
 
-include $(CORE_DEPTH)/coreconf/UNIX.mk
-
-DEFAULT_COMPILER = cc
-
-ifeq ($(OS_TEST),ia64)
-	CPU_ARCH = ia64
-	CPU_TAG  = _$(CPU_ARCH)
-	ifneq ($(USE_64), 1)
-		64BIT_TAG=_32
-	endif
-	DLL_SUFFIX = so
-else
-	CPU_ARCH = hppa
-	DLL_SUFFIX = sl
-endif
-CC         = cc
-CCC        = CC
-OS_CFLAGS  += -Ae $(DSO_CFLAGS) -DHPUX -D$(CPU_ARCH) -D_HPUX_SOURCE -D_USE_BIG_FDS
-
-ifeq ($(DEFAULT_IMPL_STRATEGY),_PTH)
-	USE_PTHREADS = 1
-	ifeq ($(CLASSIC_NSPR),1)
-		USE_PTHREADS =
-		IMPL_STRATEGY = _CLASSIC
-	endif
-	ifeq ($(PTHREADS_USER),1)
-		USE_PTHREADS =
-		IMPL_STRATEGY = _PTH_USER
-	endif
+ifeq ($(OS_RELEASE),B.11.23)
+OS_CFLAGS		+= -DHPUX10
+DEFAULT_IMPL_STRATEGY = _PTH
 endif
 
-ifdef PTHREADS_USER
+#
+# To use the true pthread (kernel thread) library on 10.30 and
+# 11.x, we should define _POSIX_C_SOURCE to be 199506L.
+# The _REENTRANT macro is deprecated.
+#
+
+ifdef USE_PTHREADS
 	OS_CFLAGS	+= -D_POSIX_C_SOURCE=199506L
 endif
 
-LDFLAGS			= -z -Wl,+s
-
-MKSHLIB			= $(LD) $(DSO_LDOPTS)
-ifdef MAPFILE
-MKSHLIB += -c $(MAPFILE)
-endif
-PROCESS_MAP_FILE = grep -v ';+' $(LIBRARY_NAME).def | grep -v ';-' | \
-         sed -e 's; DATA ;;' -e 's,;;,,' -e 's,;.*,,' -e 's,^,+e ,' > $@
-
-DSO_LDOPTS		= -b +h $(notdir $@)
-ifeq ($(OS_TEST),ia64)
-	DSO_LDOPTS	+= +b '$$ORIGIN'
-endif
-DSO_LDFLAGS		=
-
-# +Z generates position independent code for use in shared libraries.
-DSO_CFLAGS = +Z
+#
+# Config stuff for HP-UXB.11.x.
+#
+include $(CORE_DEPTH)/coreconf/HP-UXB.11.mk
