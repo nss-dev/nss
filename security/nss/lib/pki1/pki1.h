5e7ef4541eda8b78b794ce06041718854ee456af
wtchang%redhat.com <none@none>
2006-02-02 00:20:07 +0000

    Bugzilla Bug 284386: fixed build error using gcc4.  gcc4 disallows arrays of incomplete element type (which are invalid in C).  We moved the declaration of nss_builtin_oids to pki1.h, after the definition of NSSOID. r=caillon. a=dveditz for approval1.7.13. Modified Files:  Tag: MOZILLA_1_7_BRANCH 	oiddata.c oiddata.h oidgen.perl pki1.h
    --HG--
    branch : MOZILLA_1_7_BRANCH

--- security/nss/lib/pki1/pki1.h
+++ security/nss/lib/pki1/pki1.h
@@ -55,7 +55,9 @@ static const char PKI1_CVS_ID[] = "@(#) $RCSfile$ $Revision$ $Date$ $Name$";
 
 PR_BEGIN_EXTERN_C
 
-/* fgmr 19990505 moved these here from oiddata.h */
+extern const NSSOID nss_builtin_oids[];
+extern const PRUint32 nss_builtin_oid_count;
+
 extern const nssAttributeTypeAliasTable nss_attribute_type_aliases[];
 extern const PRUint32 nss_attribute_type_alias_count;
 
