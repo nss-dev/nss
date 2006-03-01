826c305dd33685b0a9f9d1af072b43906166d022
rrelyea%redhat.com <none@none>
2006-03-01 21:42:56 +0000

    Bugzilla Bug 326482 NSS ECC performance problems. Patch by Nelson, r=relyea., r=wtchang
    Save the public key when we create the keypair so we can use it later.
    
    --HG--
    branch : NSS_3_11_BRANCH

--- security/nss/lib/ssl/sslsecur.c
+++ security/nss/lib/ssl/sslsecur.c
@@ -632,6 +632,7 @@ SSL_ConfigSecureServer(PRFileDesc *fd, CERTCertificate *cert,
     SECStatus rv;
     sslSocket *ss;
     sslServerCerts  *sc;
+    SECKEYPublicKey * pubKey = NULL;
 
     ss = ssl_FindSocket(fd);
     if (!ss) {
@@ -664,7 +665,6 @@ SSL_ConfigSecureServer(PRFileDesc *fd, CERTCertificate *cert,
     	sc->serverCert = NULL;
     }
     if (cert) {
-	SECKEYPublicKey * pubKey;
 	sc->serverCert = CERT_DupCertificate(cert);
 	if (!sc->serverCert)
 	    goto loser;
@@ -673,8 +673,6 @@ SSL_ConfigSecureServer(PRFileDesc *fd, CERTCertificate *cert,
 	if (!pubKey) 
 	    goto loser;
 	sc->serverKeyBits = SECKEY_PublicKeyStrengthInBits(pubKey);
-	SECKEY_DestroyPublicKey(pubKey); 
-	pubKey = NULL;
     }
 
 
@@ -723,11 +721,12 @@ SSL_ConfigSecureServer(PRFileDesc *fd, CERTCertificate *cert,
 	if (keyCopy == NULL)
 	    goto loser;
 	SECKEY_CacheStaticFlags(keyCopy);
-        sc->serverKeyPair = ssl3_NewKeyPair(keyCopy, NULL);
+        sc->serverKeyPair = ssl3_NewKeyPair(keyCopy, pubKey);
         if (sc->serverKeyPair == NULL) {
             SECKEY_DestroyPrivateKey(keyCopy);
             goto loser;
         }
+	pubKey = NULL; /* adopted by serverKeyPair */
     }
 
     if (kea == kt_rsa && cert && sc->serverKeyBits > 512) {
@@ -748,6 +747,10 @@ SSL_ConfigSecureServer(PRFileDesc *fd, CERTCertificate *cert,
     return SECSuccess;
 
 loser:
+    if (pubKey) {
+	SECKEY_DestroyPublicKey(pubKey); 
+	pubKey = NULL;
+    }
     if (sc->serverCert != NULL) {
 	CERT_DestroyCertificate(sc->serverCert);
 	sc->serverCert = NULL;
