4becff343cb5c477ea8adfe6bb9fcf48676c8ecc
rrelyea%redhat.com <none@none>
2006-05-15 21:55:47 +0000

    Bug 305835: Remove NSS_ENABLE_ECC ifdefs in libssl
    --HG--
    branch : NSS_3_11_BRANCH

--- security/nss/lib/ssl/sslproto.h
+++ security/nss/lib/ssl/sslproto.h
@@ -165,7 +165,6 @@
 #define TLS_DHE_DSS_EXPORT1024_WITH_RC4_56_SHA  0x0065
 #define TLS_DHE_DSS_WITH_RC4_128_SHA            0x0066
 
-#ifdef NSS_ENABLE_ECC
 #define TLS_ECDH_ECDSA_WITH_NULL_SHA            0xC001
 #define TLS_ECDH_ECDSA_WITH_RC4_128_SHA         0xC002
 #define TLS_ECDH_ECDSA_WITH_3DES_EDE_CBC_SHA    0xC003
@@ -195,7 +194,6 @@
 #define TLS_ECDH_anon_WITH_3DES_EDE_CBC_SHA     0xC017
 #define TLS_ECDH_anon_WITH_AES_128_CBC_SHA      0xC018
 #define TLS_ECDH_anon_WITH_AES_256_CBC_SHA      0xC019
-#endif /* NSS_ENABLE_ECC */
 
 /* Netscape "experimental" cipher suites. */
 #define SSL_RSA_OLDFIPS_WITH_3DES_EDE_CBC_SHA	0xffe0
