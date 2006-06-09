361f26c2a8ccd827ed250ef809d4f29d2c665a83
nelson%bolyard.com <none@none>
2006-06-09 18:10:02 +0000

    Fix broken build.
    --HG--
    branch : NSS_3_11_BRANCH

--- security/nss/cmd/certutil/certutil.c
+++ security/nss/cmd/certutil/certutil.c
@@ -1568,7 +1568,7 @@ AddOidToSequence(CERTOidSequence *os, SECOidTag oidTag)
   return SECSuccess;
 }
 
-SEC_ASN1_MKSUB(SEC_ObjectIDTemplate);
+SEC_ASN1_MKSUB(SEC_ObjectIDTemplate)
 
 const SEC_ASN1Template CERT_OidSeqTemplate[] = {
     { SEC_ASN1_SEQUENCE_OF | SEC_ASN1_XTRN,
@@ -3085,6 +3085,7 @@ shutdown:
 
     if ((SECSuccess == rv) && certutil.commands[cmd_Batch].activated) {
 	FILE* batchFile = NULL;
+        char nextcommand[512];
         if (!certutil.options[opt_InputFile].activated ||
             !certutil.options[opt_InputFile].arg) {
 	    PR_fprintf(PR_STDERR,
@@ -3093,7 +3094,6 @@ shutdown:
 	    return 255;
         }
         batchFile = fopen(certutil.options[opt_InputFile].arg, "r");
-        char nextcommand[512];
         if (!batchFile) {
 	    PR_fprintf(PR_STDERR,
 	               "%s:  unable to open \"%s\" for reading (%ld, %ld).\n",
