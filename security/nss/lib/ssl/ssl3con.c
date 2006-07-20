3ec1e6c416316b2ee32d471c98af934f8dbb624a
nelson%bolyard.com <none@none>
2006-07-20 00:17:23 +0000

    Correct ifdefs so that non-ECC builds will continue to build correctly. r=wtchang  bug 341707.

--- security/nss/lib/ssl/ssl3con.c
+++ security/nss/lib/ssl/ssl3con.c
@@ -3507,7 +3507,7 @@ ssl3_SendClientHello(sslSocket *ss)
 	if (total_exten_len > 0)
 	    total_exten_len += 2;
     }
-#ifndef NSS_ECC_MORE_THAN_SUITE_B
+#if defined(NSS_ENABLE_ECC) && !defined(NSS_ECC_MORE_THAN_SUITE_B)
     else { /* SSL3 only */
     	ssl3_DisableECCSuites(ss, NULL); /* disable all ECC suites */
     }
