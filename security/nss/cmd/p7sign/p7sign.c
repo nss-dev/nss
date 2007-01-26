0ca5a3e14cf33bb15003220f220d4a777f0008b1
nelson%bolyard.com <none@none>
2007-01-26 01:15:43 +0000

    Open output file in binary mode for writing binary PKCS7 message. Bug 301496. Patch by Alexei.Volkov, r=nelson

--- security/nss/cmd/p7sign/p7sign.c
+++ security/nss/cmd/p7sign/p7sign.c
@@ -238,7 +238,7 @@ main(int argc, char **argv)
 	    break;
 
 	  case 'o':
-	    outFile = fopen(optstate->value, "w");
+	    outFile = fopen(optstate->value, "wb");
 	    if (!outFile) {
 		fprintf(stderr, "%s: unable to open \"%s\" for writing\n",
 			progName, optstate->value);
