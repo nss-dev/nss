07909142abbbbaecffe2cfc0a1fcf8eeaa5a78ad
julien.pierre.boogz%sun.com <none@none>
2007-11-15 22:05:01 +0000

    Fix for bug 399304 . Replace use of nss_InitLock with PZ_NewLock . Patch contributed by Wan-Teh . r=julien,nelson
    --HG--
    branch : NSS_3_11_BRANCH

--- security/nss/lib/certdb/certdb.c
+++ security/nss/lib/certdb/certdb.c
@@ -2782,7 +2782,7 @@ SECStatus
 cert_InitLocks(void)
 {
     if ( certRefCountLock == NULL ) {
-        nss_InitLock(&certRefCountLock, nssILockRefLock);
+        certRefCountLock = PZ_NewLock(nssILockRefLock);
         PORT_Assert(certRefCountLock != NULL);
         if (!certRefCountLock) {
             return SECFailure;
@@ -2790,7 +2790,7 @@ cert_InitLocks(void)
     }
 
     if ( certTrustLock == NULL ) {
-        nss_InitLock(&certTrustLock, nssILockCertDB);
+        certTrustLock = PZ_NewLock(nssILockCertDB);
         PORT_Assert(certTrustLock != NULL);
         if (!certTrustLock) {
             PZ_DestroyLock(certRefCountLock);
