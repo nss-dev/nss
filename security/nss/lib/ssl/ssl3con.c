1fb56054971577e1853854b0a164fed414ec057e
julien.pierre.boogz%sun.com <none@none>
2008-02-13 02:43:08 +0000

    Fix for bug 398680 . Assertion in ssl3_RegisterServerHelloExtensionsSender. memset array before handshake. r=nelson,alexei
    --HG--
    branch : NSS_3_11_BRANCH

--- security/nss/lib/ssl/ssl3con.c
+++ security/nss/lib/ssl/ssl3con.c
@@ -5504,6 +5504,8 @@ ssl3_HandleClientHello(sslSocket *ss, SSL3Opaque *b, PRUint32 length)
 	return rv;		/* error code is set. */
     }
 
+    memset(&ss->serverExtensionSenders[0], 0,
+           sizeof ss->serverExtensionSenders);
     rv = ssl3_InitState(ss);
     if (rv != SECSuccess) {
 	return rv;		/* ssl3_InitState has set the error code. */
@@ -5947,6 +5949,9 @@ ssl3_HandleV2ClientHello(sslSocket *ss, unsigned char *buffer, int length)
     PORT_Assert( ss->opt.noLocks || ssl_HaveRecvBufLock(ss) );
 
     ssl_GetSSL3HandshakeLock(ss);
+
+    memset(&ss->serverExtensionSenders[0], 0,
+           sizeof ss->serverExtensionSenders);
 
     rv = ssl3_InitState(ss);
     if (rv != SECSuccess) {
