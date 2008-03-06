c4384d9ac957fbe80362b17c78f4c54c5429b01a
wtc%google.com <>
2008-03-06 20:16:24 +0000

    Bug 403563: implement the TLS session ticket extension (rfc4507bis).  The patch is contributed by Nagendra Modadugu <ngm+mozilla@google.com>.  A small portion (PKCS #11 code and tests) was written by Wan-Teh Chang <wtc@google.com>.  r=nelson,wtc Modified Files: 	cmd/lib/SSLerrs.h cmd/selfserv/selfserv.c 	cmd/strsclnt/strsclnt.c cmd/tstclnt/tstclnt.c 	lib/ssl/manifest.mn lib/ssl/ssl.h lib/ssl/ssl3con.c 	lib/ssl/ssl3ecc.c lib/ssl/ssl3prot.h lib/ssl/sslerr.h 	lib/ssl/sslimpl.h lib/ssl/sslnonce.c lib/ssl/sslsnce.c 	lib/ssl/sslsock.c lib/ssl/sslt.h tests/ssl/sslstress.txt Added Files: 	lib/ssl/ssl3ext.c

--- security/nss/lib/ssl/sslt.h
+++ security/nss/lib/ssl/sslt.h
@@ -59,6 +59,12 @@ typedef struct SSL3StatisticsStr {
     long hch_sid_cache_hits;
     long hch_sid_cache_misses;
     long hch_sid_cache_not_ok;
+
+    /* statistics related to stateless resume */
+    long sch_sid_stateless_resumes;
+    long hsh_sid_stateless_resumes;
+    long hch_sid_stateless_resumes;
+    long hch_sid_ticket_parse_failures;
 } SSL3Statistics;
 
 /* Key Exchange algorithm values */
