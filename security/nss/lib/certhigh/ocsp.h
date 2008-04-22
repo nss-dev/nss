0741d9f0437a9a913e90d5c671f694c21deb1c01
kaie%kuix.de <>
2008-04-22 15:59:26 +0000

    Bug 428105, CERT_SetOCSPTimeout is not defined in any public header file r=nelson

--- security/nss/lib/certhigh/ocsp.h
+++ security/nss/lib/certhigh/ocsp.h
@@ -100,6 +100,12 @@ extern SECStatus
 CERT_SetOCSPFailureMode(SEC_OcspFailureMode ocspFailureMode);
 
 /*
+ * Configure the maximum time NSS will wait for an OCSP response.
+ */
+extern SECStatus
+CERT_SetOCSPTimeout(PRUint32 seconds);
+
+/*
  * Removes all items currently stored in the OCSP cache.
  */
 extern SECStatus
