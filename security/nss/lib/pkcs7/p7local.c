3621e16a86d75a9eaccf8787eedc2d861851b835
nelson%bolyard.com <>
2008-05-30 03:39:46 +0000

    Bug 436428: remove unneeded assert from sec_PKCS7EncryptLength Patch by Justin Dolske <dolske@mozilla.com>, r=nelson

--- security/nss/lib/pkcs7/p7local.c
+++ security/nss/lib/pkcs7/p7local.c
@@ -585,7 +585,6 @@ sec_PKCS7Decrypt (sec_PKCS7CipherObject *obj, unsigned char *output,
      */
     if (final && (padsize != 0)) {
 	unsigned int padlen = *(output + ofraglen - 1);
-	PORT_Assert (padlen > 0 && padlen <= padsize);
 	if (padlen == 0 || padlen > padsize) {
 	    PORT_SetError (SEC_ERROR_BAD_DATA);
 	    return SECFailure;
