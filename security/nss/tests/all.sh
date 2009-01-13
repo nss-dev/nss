6008ee17a4eea8bcf66fab2b4af9276a47505323
slavomir.katuscak%sun.com <>
2009-01-13 18:50:29 +0000

    Bug 472682 - Removed PKIX chains tests from upgrade DB test cycle. r=christophe

--- security/nss/tests/all.sh
+++ security/nss/tests/all.sh
@@ -64,6 +64,7 @@
 #   ocsp.sh      - OCSP testing
 #   merge.sh     - tests merging old and new shareable databases
 #   pkits.sh     - NIST/PKITS tests 
+#   chains.sh    - PKIX cert chains tests 
 #   dbupgrade.sh - upgrade databases to new shareable version (used
 #                  only in upgrade test cycle)
 #   memleak.sh   - memory leak testing (optional)
@@ -233,7 +234,7 @@ run_cycle_upgrade_db()
 
     # run the subset of tests with the upgraded database
     TESTS="${ALL_TESTS}"
-    TESTS_SKIP="cipher libpkix cert dbtests sdr ocsp pkits"
+    TESTS_SKIP="cipher libpkix cert dbtests sdr ocsp pkits chains"
 
     echo "${NSS_SSL_TESTS}" | grep "_" > /dev/null
     RET=$?
