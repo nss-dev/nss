a17229a334f4659932673c4e3e54249ed6016fb4
nelson%bolyard.com <>
2009-03-12 08:53:51 +0000

    The patch for bug 159483 was contributed by Kaspar Brand <mozbugzilla@velox.ch>.

--- security/nss/lib/certdb/certdb.c
+++ security/nss/lib/certdb/certdb.c
@@ -20,6 +20,7 @@
  *
  * Contributor(s):
  *    Aaron Spangler <aaron@spangler.ods.org>
+ *    Kaspar Brand <mozbugzilla@velox.ch>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
