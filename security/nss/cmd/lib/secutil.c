70ef90be377eade4e371500be19f12a644c5db3d
nelson%bolyard.com <>
2009-03-17 08:46:24 +0000

    Back out preceding change to this file.  It broken test scripts that require quotes in the command output. :(

--- security/nss/cmd/lib/secutil.c
+++ security/nss/cmd/lib/secutil.c
@@ -998,6 +998,7 @@ secu_PrintRawString(FILE *out, SECItem *si, char *m, int level)
 	SECU_Indent(out, level); 
 	column = level*INDENT_MULT;
     }
+    fprintf(out, "\""); column++;
 
     for (i = 0; i < si->len; i++) {
 	unsigned char val = si->data[i];
@@ -1009,6 +1010,7 @@ secu_PrintRawString(FILE *out, SECItem *si, char *m, int level)
 	fprintf(out,"%c", printable[val]); column++;
     }
 
+    fprintf(out, "\""); column++;
     if (column != level*INDENT_MULT || column > 76) {
 	secu_Newline(out);
     }
