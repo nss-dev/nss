19dd1a09c6128712613e17eb86c227daf948343c
christophe.ravel.bugs%sun.com <none@none>
2009-03-27 21:42:54 +0000

    Change version to 3.12.3 RTM (for NSS 3.12.3 RC0).

--- security/nss/lib/nss/nss.h
+++ security/nss/lib/nss/nss.h
@@ -66,11 +66,11 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION  "3.12.3" _NSS_ECC_STRING " Beta" _NSS_CUSTOMIZED
+#define NSS_VERSION  "3.12.3" _NSS_ECC_STRING _NSS_CUSTOMIZED
 #define NSS_VMAJOR   3
 #define NSS_VMINOR   12
 #define NSS_VPATCH   3
-#define NSS_BETA     PR_TRUE
+#define NSS_BETA     PR_FALSE
 
 #ifndef RC_INVOKED
 
