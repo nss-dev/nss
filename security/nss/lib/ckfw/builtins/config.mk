c4247466563310ce9a04fcbd13ee7498cd35dd04
cvs2hg <>
2009-05-21 19:50:30 +0000

    fixup commit for tag 'FIREFOX_3_0_12_BUILD1'

--- security/nss/lib/ckfw/builtins/config.mk
+++ security/nss/lib/ckfw/builtins/config.mk
@@ -60,7 +60,9 @@ endif
 # To create a loadable module on Darwin, we must use -bundle.
 #
 ifeq ($(OS_TARGET),Darwin)
+ifndef USE_64
 DSO_LDOPTS = -bundle
+endif
 endif
 
 ifeq ($(OS_TARGET),SunOS)
@@ -69,3 +71,11 @@ ifeq ($(OS_TARGET),SunOS)
 MKSHLIB += -R '$$ORIGIN'
 endif
 
+ifeq ($(OS_ARCH), HP-UX) 
+ifneq ($(OS_TEST), ia64)
+# pa-risc
+ifeq ($(USE_64), 1)
+MKSHLIB += +b '$$ORIGIN'
+endif
+endif
+endif
