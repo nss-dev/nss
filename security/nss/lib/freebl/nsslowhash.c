c8a736b9bdd1913d98074d695729e60d954848fd
rrelyea%redhat.com <none@none>
2009-06-09 23:34:06 +0000

    Bug 497002 -  Lab required nspr-free freebl changes.
    nspr-free freebl needs a way to:
    1) retry the POST at a later time under application control.
    2) disable the token if the post fails.
    
    r= glen beasley

--- security/nss/lib/freebl/nsslowhash.c
+++ security/nss/lib/freebl/nsslowhash.c
@@ -289,6 +289,7 @@ static int nsslow_GetFIPSEnabled(void) {
 
 
 static int post = 0;
+static int post_failed = 0;
 
 static NSSLOWInitContext dummyContext = { 0 };
 
@@ -302,11 +303,16 @@ NSSLOW_Init(void)
 
     rv = FREEBL_InitStubs();
     nsprAvailable = (rv ==  SECSuccess ) ? PR_TRUE : PR_FALSE;
+
+    if (post_failed) {
+	return NULL;
+    }
 	
 
     if (!post && nsslow_GetFIPSEnabled()) {
 	crv = freebl_fipsPowerUpSelfTest();
 	if (crv != CKR_OK) {
+	    post_failed = 1;
 	    return NULL;
 	}
     }
@@ -323,11 +329,25 @@ NSSLOW_Shutdown(NSSLOWInitContext *context)
    return;
 }
 
+void
+NSSLOW_Reset(NSSLOWInitContext *context)
+{
+   PORT_Assert(context == &dummyContext);
+   post_failed = 0;
+   post = 0;
+   return;
+}
+
 NSSLOWHASHContext *
 NSSLOWHASH_NewContext(NSSLOWInitContext *initContext, 
 			HASH_HashType hashType)
 {
    NSSLOWHASHContext *context;
+
+   if (post_failed) {
+	PORT_SetError(SEC_ERROR_PKCS11_DEVICE_ERROR);
+	return NULL;
+   }
 
    if (initContext != &dummyContext) {
 	PORT_SetError(SEC_ERROR_INVALID_ARGS);
