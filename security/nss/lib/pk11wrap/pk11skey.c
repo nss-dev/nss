1401f0541fd619970231026d67b93f6a691dd928
rrelyea%redhat.com <none@none>
2009-06-19 01:15:22 +0000

    back out change so tree will go green.

--- security/nss/lib/pk11wrap/pk11skey.c
+++ security/nss/lib/pk11wrap/pk11skey.c
@@ -50,7 +50,6 @@
 #include "secitem.h"
 #include "secoid.h"
 #include "secerr.h"
-#include "hasht.h"
 
 /* forward static declarations. */
 static PK11SymKey *pk11_DeriveWithTemplate(PK11SymKey *baseKey, 
@@ -1637,6 +1636,7 @@ PK11_PubDerive(SECKEYPrivateKey *privKey, SECKEYPublicKey *pubKey,
 	    int templateCount;
 	    CK_ATTRIBUTE *attrs = keyTemplate;
 	    CK_ECDH1_DERIVE_PARAMS *mechParams = NULL;
+	    SECItem *pubValue = NULL;
 
 	    if (pubKey->keyType != ecKey) {
 		PORT_SetError(SEC_ERROR_BAD_KEY);
@@ -1656,18 +1656,27 @@ PK11_PubDerive(SECKEYPrivateKey *privKey, SECKEYPublicKey *pubKey,
 	    keyType = PK11_GetKeyType(target,keySize);
 	    key_size = keySize;
 	    symKey->size = keySize;
-	    if (key_size == 0) {
-		/* sigh, some tokens can't figure this out and require
-		 * CKA_VALUE_LEN to be set */
-		key_size = SHA1_LENGTH;
-	    }
+	    if (key_size == 0) templateCount--;
 
 	    mechParams = PORT_ZNew(CK_ECDH1_DERIVE_PARAMS); 
 	    mechParams->kdf = CKD_SHA1_KDF;
 	    mechParams->ulSharedDataLen = 0;
 	    mechParams->pSharedData = NULL;
-	    mechParams->ulPublicDataLen =  pubKey->u.ec.publicValue.len;
-	    mechParams->pPublicData =  pubKey->u.ec.publicValue.data;
+
+	    if (PR_GetEnv("NSS_USE_DECODED_CKA_EC_POINT")) {
+		mechParams->ulPublicDataLen =  pubKey->u.ec.publicValue.len;
+		mechParams->pPublicData =  pubKey->u.ec.publicValue.data;
+	    } else {
+		pubValue = SEC_ASN1EncodeItem(NULL, NULL,
+			&pubKey->u.ec.publicValue,
+			SEC_ASN1_GET(SEC_OctetStringTemplate));
+		if (pubValue == NULL) {
+	    	    PORT_ZFree(mechParams, sizeof(CK_ECDH1_DERIVE_PARAMS));
+		    break;
+		}
+		mechParams->ulPublicDataLen =  pubValue->len;
+		mechParams->pPublicData =  pubValue->data;
+	    }
 
 	    mechanism.mechanism = derive;
 	    mechanism.pParameter = mechParams;
@@ -1679,28 +1688,9 @@ PK11_PubDerive(SECKEYPrivateKey *privKey, SECKEYPublicKey *pubKey,
 		templateCount, &symKey->objectID);
 	    pk11_ExitKeyMonitor(symKey);
 
-	    /* old PKCS #11 spec was ambiguous on what needed to be passed,
-	     * try this again with and encoded public key */
-	    if (crv != CKR_OK) {
-		SECItem *pubValue = SEC_ASN1EncodeItem(NULL, NULL,
-			&pubKey->u.ec.publicValue,
-			SEC_ASN1_GET(SEC_OctetStringTemplate));
-		if (pubValue == NULL) {
-	    	    PORT_ZFree(mechParams, sizeof(CK_ECDH1_DERIVE_PARAMS));
-		    break;
-		}
-		mechParams->ulPublicDataLen =  pubValue->len;
-		mechParams->pPublicData =  pubValue->data;
-
-		pk11_EnterKeyMonitor(symKey);
-		crv = PK11_GETTAB(slot)->C_DeriveKey(symKey->session, 
-		    &mechanism, privKey->pkcs11ID, keyTemplate, 
-		    templateCount, &symKey->objectID);
-		pk11_ExitKeyMonitor(symKey);
-
+	    if (pubValue) {
 		SECITEM_FreeItem(pubValue,PR_TRUE);
 	    }
-
 	    PORT_ZFree(mechParams, sizeof(CK_ECDH1_DERIVE_PARAMS));
 
 	    if (crv == CKR_OK) return symKey;
@@ -1732,6 +1722,7 @@ pk11_PubDeriveECKeyWithKDF(
     int                     templateCount;
     CK_ATTRIBUTE           *attrs           = keyTemplate;
     CK_ECDH1_DERIVE_PARAMS *mechParams      = NULL;
+    SECItem *pubValue = NULL;
 
     if (pubKey->keyType != ecKey) {
 	PORT_SetError(SEC_ERROR_BAD_KEY);
@@ -1760,22 +1751,8 @@ pk11_PubDeriveECKeyWithKDF(
     keyType = PK11_GetKeyType(target,keySize);
     key_size = keySize;
     symKey->size = keySize;
-    if (key_size == 0) {
-	/* sigh, some tokens can't figure this out and require
-	 * CKA_VALUE_LEN to be set */
-	switch (kdf) {
-	case CKD_NULL:
-	    key_size = (pubKey->u.ec.publicValue.len-1)/2;
-	    break;
-	case CKD_SHA1_KDF:
-	    key_size = SHA1_LENGTH;
-	    break;
-	default:
-	    PORT_Assert("Invalid CKD");
-	    PORT_SetError(SEC_ERROR_INVALID_ALGORITHM);
-	    return NULL;
-	}
-    }
+    if (key_size == 0) 
+    	templateCount--;
 
     mechParams = PORT_ZNew(CK_ECDH1_DERIVE_PARAMS);
     if (!mechParams) {
@@ -1790,8 +1767,21 @@ pk11_PubDeriveECKeyWithKDF(
 	mechParams->ulSharedDataLen = sharedData->len;
 	mechParams->pSharedData     = sharedData->data;
     }
-    mechParams->ulPublicDataLen =  pubKey->u.ec.publicValue.len;
-    mechParams->pPublicData =  pubKey->u.ec.publicValue.data;
+    if (PR_GetEnv("NSS_USE_DECODED_CKA_EC_POINT")) {
+	mechParams->ulPublicDataLen =  pubKey->u.ec.publicValue.len;
+	mechParams->pPublicData =  pubKey->u.ec.publicValue.data;
+    } else {
+	pubValue = SEC_ASN1EncodeItem(NULL, NULL,
+		&pubKey->u.ec.publicValue,
+		SEC_ASN1_GET(SEC_OctetStringTemplate));
+	if (pubValue == NULL) {
+    	    PORT_ZFree(mechParams, sizeof(CK_ECDH1_DERIVE_PARAMS));
+	    PK11_FreeSymKey(symKey);
+	    return NULL;
+	}
+	mechParams->ulPublicDataLen =  pubValue->len;
+	mechParams->pPublicData =  pubValue->data;
+    }
 
     mechanism.mechanism      = derive;
     mechanism.pParameter     = mechParams;
@@ -1802,29 +1792,10 @@ pk11_PubDeriveECKeyWithKDF(
     	privKey->pkcs11ID, keyTemplate, templateCount, &symKey->objectID);
     pk11_ExitKeyMonitor(symKey);
 
-    /* old PKCS #11 spec was ambiguous on what needed to be passed,
-     * try this again with and encoded public key */
-    if (crv != CKR_OK) {
-	SECItem *pubValue = SEC_ASN1EncodeItem(NULL, NULL,
-		&pubKey->u.ec.publicValue,
-		SEC_ASN1_GET(SEC_OctetStringTemplate));
-	if (pubValue == NULL) {
-	    goto loser;
-	}
-	mechParams->ulPublicDataLen =  pubValue->len;
-	mechParams->pPublicData =  pubValue->data;
-
-	pk11_EnterKeyMonitor(symKey);
-	crv = PK11_GETTAB(slot)->C_DeriveKey(symKey->session, 
-	    &mechanism, privKey->pkcs11ID, keyTemplate, 
-	    templateCount, &symKey->objectID);
-	pk11_ExitKeyMonitor(symKey);
-
+    PORT_ZFree(mechParams, sizeof(CK_ECDH1_DERIVE_PARAMS));
+    if (pubValue) {
 	SECITEM_FreeItem(pubValue,PR_TRUE);
     }
-
-loser:
-    PORT_ZFree(mechParams, sizeof(CK_ECDH1_DERIVE_PARAMS));
 
     if (crv != CKR_OK) {
 	PK11_FreeSymKey(symKey);
