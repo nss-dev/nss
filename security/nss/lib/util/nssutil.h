a538884d9d22b2be456d718d84a1a256617b086a
christophe.ravel.bugs%sun.com <none@none>
2009-08-13 18:31:50 +0000

    Version on the NSS trunk is now 3.12.5 Beta.

--- security/nss/lib/util/nssutil.h
+++ security/nss/lib/util/nssutil.h
@@ -51,10 +51,10 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>][ <Beta>]"
  */
-#define NSSUTIL_VERSION  "3.12.4.5"
+#define NSSUTIL_VERSION  "3.12.5.0 Beta"
 #define NSSUTIL_VMAJOR   3
 #define NSSUTIL_VMINOR   12
-#define NSSUTIL_VPATCH   4
-#define NSSUTIL_BETA     PR_FALSE
+#define NSSUTIL_VPATCH   5
+#define NSSUTIL_BETA     PR_TRUE
 
 #endif /* __nssutil_h_ */
