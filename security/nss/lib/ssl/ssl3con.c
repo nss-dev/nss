0eff83f06f761b5663a9dd2c380dcc772dd5eea8
wtc%google.com <>
2009-12-01 17:59:46 +0000

    Bug 531188: Don't call zlib with 0-length inputs (the avail_in is 0), otherwise zlib returns Z_BUF_ERROR, which we consider fatal.  The patch is contributed by Adam Langley of Google <agl@chromium.org>.  r=wtc,nelson.

--- security/nss/lib/ssl/ssl3con.c
+++ security/nss/lib/ssl/ssl3con.c
@@ -1295,7 +1295,7 @@ ssl3_MapZlibError(int zlib_error)
 static SECStatus
 ssl3_DeflateInit(void *void_context)
 {
-    z_stream* context = void_context;
+    z_stream *context = void_context;
     context->zalloc = NULL;
     context->zfree = NULL;
     context->opaque = NULL;
@@ -1306,7 +1306,7 @@ ssl3_DeflateInit(void *void_context)
 static SECStatus
 ssl3_InflateInit(void *void_context)
 {
-    z_stream* context = void_context;
+    z_stream *context = void_context;
     context->zalloc = NULL;
     context->zfree = NULL;
     context->opaque = NULL;
@@ -1320,7 +1320,13 @@ static SECStatus
 ssl3_DeflateCompress(void *void_context, unsigned char *out, int *out_len,
                      int maxout, const unsigned char *in, int inlen)
 {
-    z_stream* context = void_context;
+    z_stream *context = void_context;
+
+    if (!inlen) {
+        *out_len = 0;
+        return SECSuccess;
+    }
+
     context->next_in = (unsigned char*) in;
     context->avail_in = inlen;
     context->next_out = out;
@@ -1343,7 +1349,13 @@ static SECStatus
 ssl3_DeflateDecompress(void *void_context, unsigned char *out, int *out_len,
                        int maxout, const unsigned char *in, int inlen)
 {
-    z_stream* context = void_context;
+    z_stream *context = void_context;
+
+    if (!inlen) {
+        *out_len = 0;
+        return SECSuccess;
+    }
+
     context->next_in = (unsigned char*) in;
     context->avail_in = inlen;
     context->next_out = out;
