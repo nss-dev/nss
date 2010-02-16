58c09a02cf0abe73945ef5bd05cf4b9547ab59f9
wtc%google.com <none@none>
2010-02-16 18:56:48 +0000

    Bug 537356: Rename SCSV.  In the final RFC, the symbolic name of the SCSV changed to TLS_EMPTY_RENEGOTIATION_INFO_SCSV.  r=christophe,rrelyea. Modified Files: 	cmd/ssltap/ssltap.c lib/ssl/ssl.h lib/ssl/ssl3con.c 	lib/ssl/sslproto.h

--- security/nss/lib/ssl/ssl.h
+++ security/nss/lib/ssl/ssl.h
@@ -123,7 +123,7 @@ SSL_IMPORT PRFileDesc *SSL_ImportFD(PRFileDesc *model, PRFileDesc *fd);
 #define SSL_ENABLE_DEFLATE             19 /* Enable TLS compression with    */
                                           /* DEFLATE (off by default)       */
 #define SSL_ENABLE_RENEGOTIATION       20 /* Values below (default: never)  */
-#define SSL_REQUIRE_SAFE_NEGOTIATION   21 /* Peer must send Signalling      */
+#define SSL_REQUIRE_SAFE_NEGOTIATION   21 /* Peer must send Signaling       */
 					  /* Cipher Suite Value (SCSV) or   */
                                           /* Renegotiation  Info (RI)       */
 					  /* extension in ALL handshakes.   */
