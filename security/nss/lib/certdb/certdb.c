0058ac9f243852b092fd416b0a12761b670906a6
nelson%bolyard.com <>
2010-04-25 00:44:55 +0000

    Bug 554425: Remove support for Netscape's SSL server name extension r=rrelyea@redhat.com,matt@mattmccutchen.net

--- security/nss/lib/certdb/certdb.c
+++ security/nss/lib/certdb/certdb.c
@@ -1829,13 +1829,7 @@ CERT_GetValidDNSPatternsFromCert(CERTCertificate *cert)
     }
 
     /* no SAN extension or no names found in extension */
-    /* now try the NS cert name extension first, then the common name */
-    singleName = 
-      CERT_FindNSStringExtension(cert, SEC_OID_NS_CERT_EXT_SSL_SERVER_NAME);
-    if (!singleName) {
-      singleName = CERT_GetCommonName(&cert->subject);
-    }
-
+    singleName = CERT_GetCommonName(&cert->subject);
     if (singleName) {
       nickNames->numnicknames = 1;
       nickNames->nicknames = PORT_ArenaAlloc(arena, sizeof(char *));
@@ -1884,11 +1878,7 @@ CERT_VerifyCertName(CERTCertificate *cert, const char *hn)
     if (rv == SECSuccess || PORT_GetError() != SEC_ERROR_EXTENSION_NOT_FOUND)
     	return rv;
 
-    /* try the cert extension first, then the common name */
-    cn = CERT_FindNSStringExtension(cert, SEC_OID_NS_CERT_EXT_SSL_SERVER_NAME);
-    if ( !cn ) {
-	cn = CERT_GetCommonName(&cert->subject);
-    }
+    cn = CERT_GetCommonName(&cert->subject);
     if ( cn ) {
 	rv = cert_TestHostName(cn, hn);
 	PORT_Free(cn);
