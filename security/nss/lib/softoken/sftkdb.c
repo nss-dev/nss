307ebfc60a58c0ca2316a1647cf350920ea14ab3
wtc%google.com <none@none>
2010-06-23 23:49:59 +0000

    Bug 564011: When signing attributes to a peer db, remember to create a transaction.  The patch is contributed by David Benjamin of Google <davidben@google.com>.  r=wtc.  Tag: SOFTOKEN_3_13_BRANCH
    --HG--
    branch : SOFTOKEN_3_13_BRANCH

--- security/nss/lib/softoken/sftkdb.c
+++ security/nss/lib/softoken/sftkdb.c
@@ -509,18 +509,23 @@ sftk_signTemplate(PLArenaPool *arena, SFTKDBHandle *handle,
 		  CK_ULONG count)
 {
     int i;
+    CK_RV crv;
     SFTKDBHandle *keyHandle = handle;
     SDB *keyTarget = NULL;
+    PRBool usingPeerDB = PR_FALSE;
+    PRBool inPeerDBTransaction = PR_FALSE;
 
     PORT_Assert(handle);
 
     if (handle->type != SFTK_KEYDB_TYPE) {
 	keyHandle = handle->peerDB;
+	usingPeerDB = PR_TRUE;
     }
 
     /* no key DB defined? then no need to sign anything */
     if (keyHandle == NULL) {
-	return CKR_OK;
+	crv = CKR_OK;
+	goto loser;
     }
 
     /* When we are in a middle of an update, we have an update database set, 
@@ -532,7 +537,17 @@ sftk_signTemplate(PLArenaPool *arena, SFTKDBHandle *handle,
 
     /* skip the the database does not support meta data */
     if ((keyTarget->sdb_flags & SDB_HAS_META) == 0) {
-	return CKR_OK;
+	crv = CKR_OK;
+	goto loser;
+    }
+
+    /* If we had to switch databases, we need to initialize a transaction. */
+    if (usingPeerDB) {
+	crv = (*keyTarget->sdb_Begin)(keyTarget);
+	if (crv != CKR_OK) {
+	    goto loser;
+	}
+	inPeerDBTransaction = PR_TRUE;
     }
 
     for (i=0; i < count; i ++) {
@@ -546,23 +561,44 @@ sftk_signTemplate(PLArenaPool *arena, SFTKDBHandle *handle,
 	    PZ_Lock(keyHandle->passwordLock);
 	    if (keyHandle->passwordKey.data == NULL) {
 		PZ_Unlock(keyHandle->passwordLock);
-		return CKR_USER_NOT_LOGGED_IN;
+		crv = CKR_USER_NOT_LOGGED_IN;
+		goto loser;
 	    }
 	    rv = sftkdb_SignAttribute(arena, &keyHandle->passwordKey, 
 				objectID, template[i].type,
 				&plainText, &signText);
 	    PZ_Unlock(keyHandle->passwordLock);
 	    if (rv != SECSuccess) {
-		return CKR_GENERAL_ERROR; /* better error code here? */
+		crv = CKR_GENERAL_ERROR; /* better error code here? */
+		goto loser;
 	    }
 	    rv = sftkdb_PutAttributeSignature(handle, keyTarget, 
 				objectID, template[i].type, signText);
 	    if (rv != SECSuccess) {
-		return CKR_GENERAL_ERROR; /* better error code here? */
+		crv = CKR_GENERAL_ERROR; /* better error code here? */
+		goto loser;
 	    }
 	}
     }
-    return CKR_OK;
+    crv = CKR_OK;
+
+    /* If necessary, commit the transaction */
+    if (inPeerDBTransaction) {
+	crv = (*keyTarget->sdb_Commit)(keyTarget);
+	if (crv != CKR_OK) {
+	    goto loser;
+	}
+	inPeerDBTransaction = PR_FALSE;
+    }
+
+loser:
+    if (inPeerDBTransaction) {
+	/* The transaction must have failed. Abort. */
+	(*keyTarget->sdb_Abort)(keyTarget);
+	PORT_Assert(crv != CKR_OK);
+	if (crv == CKR_OK) crv = CKR_GENERAL_ERROR;
+    }
+    return crv;
 }
 
 static CK_RV
