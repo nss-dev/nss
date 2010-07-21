5428fdb62b51a58200c6055f3ed2188437d84b8b
wtc%google.com <>
2010-07-21 16:23:17 +0000

    Bug: Don't use NO_CHECK_FORK in NSS 3.13 or later.  Use NO_FORK_CHECK instead.  r=christophe. Modified Files: 	coreconf/Linux2.4.mk nss/lib/freebl/blapii.h

--- security/coreconf/Linux2.4.mk
+++ security/coreconf/Linux2.4.mk
@@ -46,8 +46,4 @@ endif
 PROCESS_MAP_FILE = grep -v ';-' $< | \
         sed -e 's,;+,,' -e 's; DATA ;;' -e 's,;;,,' -e 's,;.*,;,' > $@
         
-# Softoken 3.13 uses NO_FORK_CHECK only.
-# Softoken 3.12 uses NO_FORK_CHECK and NO_CHECK_FORK.
-# Don't use NO_CHECK_FORK in new code.
-DEFINES += -DNO_FORK_CHECK -DNO_CHECK_FORK
-
+DEFINES += -DNO_FORK_CHECK
