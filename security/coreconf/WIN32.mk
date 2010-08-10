facfad72c8c9b4e4b8a06634e92455544b435594
wtc%google.com <none@none>
2010-08-10 14:32:55 +0000

    Bug 585847: remove -DUSE_DEBUG_RTL, which is not being used.  r=nelson.  Tag: NSS_3_12_BRANCH
    --HG--
    branch : NSS_3_12_BRANCH

--- security/coreconf/WIN32.mk
+++ security/coreconf/WIN32.mk
@@ -147,7 +147,7 @@ else # !NS_USE_GCC
 	# (RTL) in the debug build
 	#
 	ifdef USE_DEBUG_RTL
-		OS_CFLAGS += -MDd -DUSE_DEBUG_RTL -D_CRTDBG_MAP_ALLOC
+		OS_CFLAGS += -MDd -D_CRTDBG_MAP_ALLOC
 	else
 		OS_CFLAGS += -MD
 	endif
