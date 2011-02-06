62922fd125eed9bc054bec5ef7317dcde19b3f3f
nelson%bolyard.com <none@none>
2011-02-06 06:27:09 +0000

    Bug 569832: Softoken block cipher MAC'ing mechanisms aren't working Patch contributed by Konstantin Andreev <andreev@swemel.ru>, r=rrelyea

--- security/nss/lib/softoken/pkcs11i.h
+++ security/nss/lib/softoken/pkcs11i.h
@@ -266,19 +266,29 @@ typedef enum {
     SFTK_VERIFY_RECOVER
 } SFTKContextType;
 
-
+/** max block size of supported block ciphers */
 #define SFTK_MAX_BLOCK_SIZE 16
-/* currently SHA512 is the biggest hash length */
+/** currently SHA512 is the biggest hash length */
 #define SFTK_MAX_MAC_LENGTH 64
 #define SFTK_INVALID_MAC_SIZE 0xffffffff
 
+/** Particular ongoing operation in session (sign/verify/digest/encrypt/...)
+ *
+ *  Understanding sign/verify context:
+ *      multi=1 hashInfo=0   block (symmetric) cipher MACing
+ *      multi=1 hashInfo=X   PKC S/V with prior hashing
+ *      multi=0 hashInfo=0   PKC S/V one shot (w/o hashing)
+ *      multi=0 hashInfo=X   *** shouldn't happen ***
+ */
 struct SFTKSessionContextStr {
     SFTKContextType	type;
     PRBool		multi; 		/* is multipart */
     PRBool		doPad; 		/* use PKCS padding for block ciphers */
     unsigned int	blockSize; 	/* blocksize for padding */
     unsigned int	padDataLength; 	/* length of the valid data in padbuf */
+    /** latest incomplete block of data for block cipher */
     unsigned char	padBuf[SFTK_MAX_BLOCK_SIZE];
+    /** result of MAC'ing of latest full block of data with block cipher */
     unsigned char	macBuf[SFTK_MAX_BLOCK_SIZE];
     CK_ULONG		macSize;	/* size of a general block cipher mac*/
     void		*cipherInfo;
