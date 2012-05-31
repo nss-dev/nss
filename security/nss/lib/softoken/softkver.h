e5d78310ad954acd993c71baaf249b70224025bd
kaie%kuix.de <none@none>
2012-05-31 18:25:59 +0000

    Set version to NSS 3.13.5 final release, use build number 3.13.5.1 because earlier beta snapshots didn't contain "beta" in the version string
    --HG--
    branch : NSS_3_13_4_BRANCH

--- security/nss/lib/softoken/softkver.h
+++ security/nss/lib/softoken/softkver.h
@@ -57,11 +57,11 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define SOFTOKEN_VERSION  "3.13.5.0" SOFTOKEN_ECC_STRING
+#define SOFTOKEN_VERSION  "3.13.5.1" SOFTOKEN_ECC_STRING
 #define SOFTOKEN_VMAJOR   3
 #define SOFTOKEN_VMINOR   13
 #define SOFTOKEN_VPATCH   5
-#define SOFTOKEN_VBUILD   0
-#define SOFTOKEN_BETA     PR_TRUE
+#define SOFTOKEN_VBUILD   1
+#define SOFTOKEN_BETA     PR_FALSE
 
 #endif /* _SOFTKVER_H_ */
