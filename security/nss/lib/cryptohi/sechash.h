da30a73abc32f4885d0a4d642ceb4c0395581d0c
wtc%google.com <none@none>
2012-09-22 01:11:49 +0000

    Move the header include guard after the license header.

--- security/nss/lib/cryptohi/sechash.h
+++ security/nss/lib/cryptohi/sechash.h
@@ -1,9 +1,10 @@
-#ifndef _HASH_H_
-#define _HASH_H_
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 /* $Id$ */
+
+#ifndef _HASH_H_
+#define _HASH_H_
 
 #include "seccomon.h"
 #include "hasht.h"
