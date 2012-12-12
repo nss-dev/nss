46dbd5dd30fe94a9fcb72576b68a070ca5a87de2
kaie%kuix.de <>
2012-12-12 20:46:46 +0000

    Bug 762198, patch to fix the mode output when running the lowhash text, r=emaldona

--- security/nss/tests/lowhash/lowhash.sh
+++ security/nss/tests/lowhash/lowhash.sh
@@ -59,14 +59,14 @@ lowhash_test()
     TESTS="MD5 SHA1 SHA224 SHA256 SHA384 SHA512"
     OLD_MODE=`echo ${NSS_FIPS}`
     for fips_mode in 0 1; do
-      echo "lowhashtest with fips mode=${mode}"
+      echo "lowhashtest with fips mode=${fips_mode}"
       export NSS_FIPS=${fips_mode}
       for TEST in ${TESTS}
       do
         echo "lowhashtest ${TEST}"
         ${BINDIR}/lowhashtest ${TEST} 2>&1
         RESULT=$?
-        html_msg ${RESULT} 0 "lowhashtest with fips mode=${mode} for ${TEST}"
+        html_msg ${RESULT} 0 "lowhashtest with fips mode=${fips_mode} for ${TEST}"
       done
     done
     export NSS_FIPS=${OLD_MODE}
