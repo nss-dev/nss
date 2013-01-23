bcd53d4c528eb451d076c996370ce6fa191232a2
wtc%google.com <>
2013-01-23 22:08:32 +0000

    Bug 833857: Revert the change to ocspResponse_other in rev. 1.9 because some code in ocspclnt.c (the responseStatusNames array and an assertion) depends on the value of ocspResponse_other. r=kaie.

--- security/nss/lib/certhigh/ocspti.h
+++ security/nss/lib/certhigh/ocspti.h
@@ -189,14 +189,14 @@ struct CERTOCSPCertIDStr {
  * }
  */
 typedef enum {
-    ocspResponse_other = -1,		/* unknown/unrecognized value */
     ocspResponse_successful = 0,
     ocspResponse_malformedRequest = 1,
     ocspResponse_internalError = 2,
     ocspResponse_tryLater = 3,
     ocspResponse_unused = 4,
     ocspResponse_sigRequired = 5,
-    ocspResponse_unauthorized = 6
+    ocspResponse_unauthorized = 6,
+    ocspResponse_other			/* unknown/unrecognized value */
 } ocspResponseStatus;
 
 /*
