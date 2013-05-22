5befb6e357a12f75bad3acb2d573951a09058581
Wan-Teh Chang <wtc@google.com>
2013-05-21 22:21:37 -0700

    Bug 866362: Revert changeset 60da07951c17 because it caused selfserv to crash (dereferencing a null ss->ssl3.hs.kea_def) in ssl3_ServerSendStatusRequestXtn.

--- lib/ssl/ssl3ext.c
+++ lib/ssl/ssl3ext.c
@@ -682,20 +682,20 @@ ssl3_ServerSendStatusRequestXtn(
     PRInt32 extension_length;
     SECStatus rv;
     int i;
-    SECItemArray *statusToSend = NULL;
-    SSL3KEAType certIndex;
+    PRBool haveStatus = PR_FALSE;
 
-    PORT_Assert(ss->sec.isServer);
-
-    if ((ss->ssl3.hs.kea_def->kea == kea_ecdhe_rsa) ||
-	(ss->ssl3.hs.kea_def->kea == kea_dhe_rsa)) {
-	certIndex = kt_rsa;
-    } else {
-	certIndex = ss->ssl3.hs.kea_def->exchKeyType;
+    for (i = kt_null; i < kt_kea_size; i++) {
+	/* TODO: This is a temporary workaround.
+	 *       The correct code needs to see if we have an OCSP response for
+	 *       the server certificate being used, rather than if we have any
+	 *       OCSP response. See also ssl3_SendCertificateStatus.
+	 */
+	if (ss->certStatusArray[i] && ss->certStatusArray[i]->len) {
+	    haveStatus = PR_TRUE;
+	    break;
+	}
     }
-
-    statusToSend = ss->certStatusArray[certIndex];
-    if (!statusToSend || !statusToSend->len)
+    if (!haveStatus)
 	return 0;
 
     extension_length = 2 + 2;
