6584066171fb79b337a3fb76a79167c64290778c
Kai Engert <kaie@kuix.de>
2013-06-11 21:14:37 +0200

    Bug 876352 - certutil: (a) Warn if importing PEM file with private key (b) fail if user attempts to import cert with requested "u" trust, r=rrelyea

--- cmd/p7sign/p7sign.c
+++ cmd/p7sign/p7sign.c
@@ -96,7 +96,8 @@ SignFile(FILE *outFile, PRFileDesc *inFile, CERTCertificate *cert,
 	return -1;
 
     /* suck the file in */
-	if (SECU_ReadDERFromFile(&data2sign, inFile, PR_FALSE) != SECSuccess)
+	if (SECU_ReadDERFromFile(&data2sign, inFile, PR_FALSE,
+	                         PR_FALSE) != SECSuccess)
 	return -1;
 
     if (!encapsulated) {
