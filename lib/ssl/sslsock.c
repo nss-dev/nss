e4a16f7c5e7e8ae0e755fa35abf78d59cf0761a4
Ryan Sleevi <ryan.sleevi@gmail.com>
2013-08-09 14:08:06 -0700

    Bug 903565: Don't attempt to initialize the socket protocol variant when ssl_NewSocket fails to initialize the socket.
    r=wtc

--- lib/ssl/sslsock.c
+++ lib/ssl/sslsock.c
@@ -2947,6 +2947,7 @@ ssl_NewSocket(PRBool makeLocks, SSLProtocolVariant protocolVariant)
 	ss->opt.useSocks       = PR_FALSE;
 	ss->opt.noLocks        = !makeLocks;
 	ss->vrange             = *VERSIONS_DEFAULTS(protocolVariant);
+	ss->protocolVariant    = protocolVariant;
 
 	ss->peerID             = NULL;
 	ss->rTimeout	       = PR_INTERVAL_NO_TIMEOUT;
@@ -3000,7 +3001,6 @@ loser:
 	    PORT_Free(ss);
 	    ss = NULL;
 	}
-	ss->protocolVariant = protocolVariant;
     }
     return ss;
 }
