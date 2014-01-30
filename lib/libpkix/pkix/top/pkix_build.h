6548a0d4c7768c5ea9bcf3a5b65b4076d903380f
Wan-Teh Chang <wtc@google.com>
2014-01-30 12:46:34 -0800

    Bug 950129: Only call the revocation checkers during reverse path checking. Remove the unused forward builder statuses BUILD_CRL1, BUILD_CRL2PREP, and BUILD_CRL2. r=ryan.sleevi.

--- lib/libpkix/pkix/top/pkix_build.h
+++ lib/libpkix/pkix/top/pkix_build.h
@@ -29,14 +29,10 @@ typedef enum {
         BUILD_GATHERPENDING,
         BUILD_CERTVALIDATING,
         BUILD_ABANDONNODE,
-        BUILD_CRLPREP,
-        BUILD_CRL1,
         BUILD_DATEPREP,
         BUILD_CHECKTRUSTED,
         BUILD_CHECKTRUSTED2,
         BUILD_ADDTOCHAIN,
-        BUILD_CRL2PREP,
-        BUILD_CRL2,
         BUILD_VALCHAIN,
         BUILD_VALCHAIN2,
         BUILD_EXTENDCHAIN,
@@ -87,7 +83,6 @@ struct PKIX_ForwardBuilderStateStruct{
         PKIX_UInt32 numFanout;
         PKIX_UInt32 numDepth;
         PKIX_UInt32 reasonCode;
-        PKIX_Boolean revCheckDelayed;
         PKIX_Boolean canBeCached;
         PKIX_Boolean useOnlyLocal;
         PKIX_Boolean revChecking;
