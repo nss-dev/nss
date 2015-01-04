21945af059293a3660ba22992cdc4c6bb165723e
Elio Maldonado <emaldona@redhat.com>
2015-01-04 13:01:15 -0800

    Fix the handling of the case wher pkg-congig inst used
    - More work remains to be done so users don't have to edit too much
    - Tested on fedora and mint with debian
    
    --HG--
    branch : SAMPLES_BRANCH

--- samples/makesample.sh
+++ samples/makesample.sh
@@ -6,25 +6,30 @@
 # building the samples as part of a checkout of the nspr and nss
 # sources and that nss has already being built.
 #
-# Invoke with sh ./makesample.s which_sample which_target
+# Invoke with sh ./makesample.s which_sample which_target objs_name
 #
 # the first argument if the sample to build, if empty then build all
-# the second argument is th targer, either all or clean, defalt is all
+# the second argument is the target, either all or clean, defalt is all
+# the third arguments is the name of the objects directory
+# for example: Linux3.17_x86_64_glibc_PTH_64_DBG.OBJ 
 export SAMPLES="sample1 sample2 sample3 sample4 sample5 sample6"
 export ROOT_DIR=`pwd`/../..
 export DIST_DIR=${ROOT_DIR}/dist
-export BIN_DIR=${DIST_DIR}/bin
-export LIB_DIR=${DIST_DIR}/lib
+export BIN_DIR=${DIST_DIR}/$3/bin
+export LIB_DIR=${DIST_DIR}/$3/lib
+export NSPR_INC_DIR=${DIST_DIR}/$3/include
+export NSS_INC_DIR=${DIST_DIR}/public/nss
 export PATH=${PATH}:${BIN_DIR}
 export LD_LIBRARY_PATH=${LIB_DIR}
 export DONT_USE_PKG_CONFIG=1
-export TARGET=[ "$2" = "clean" ] && "clean" || "all"
+#export TARGET=[ "$2" = "clean" ] && "clean" || "all"
 if [ -z "$1" ]; then
   for d in $SAMPLES
     do
-      (cd $d; make $TARGET)
+      (cd $d; make $2)
     done
 else
-    (cd $1; gmake $2)
+    (cd $1; export NSPR_INC_DIR=$NSPR_INC_DIR; export NSS_INC_DIR=$NSS_INC_DIR; \
+     make $2)
 fi
 
