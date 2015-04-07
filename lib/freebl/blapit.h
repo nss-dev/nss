a51277f765062be9c0ceca714b0329f52bbe6b84
Martin Thomson <martin.thomson@gmail.com>
2015-04-07 13:23:09 -0700

    Bug 1138554 - Raising minimum key size on DH and RSA to 1023, r=wtc

--- lib/freebl/blapit.h
+++ lib/freebl/blapit.h
@@ -138,10 +138,10 @@ typedef int __BLAPI_DEPRECATED __attribute__((deprecated));
  * These values come from the initial key size limits from the PKCS #11
  * module. They may be arbitrarily adjusted to any value freebl supports.
  */
-#define RSA_MIN_MODULUS_BITS   128
+#define RSA_MIN_MODULUS_BITS   512
 #define RSA_MAX_MODULUS_BITS 16384
 #define RSA_MAX_EXPONENT_BITS   64
-#define DH_MIN_P_BITS	       128
+#define DH_MIN_P_BITS	      1023
 #define DH_MAX_P_BITS        16384
 
 /*
@@ -181,7 +181,7 @@ typedef int __BLAPI_DEPRECATED __attribute__((deprecated));
 
 #define DSA1_Q_BITS      160
 #define DSA_MAX_P_BITS	3072
-#define DSA_MIN_P_BITS	 512
+#define DSA_MIN_P_BITS  1023
 #define DSA_MAX_Q_BITS   256
 #define DSA_MIN_Q_BITS   160
 
