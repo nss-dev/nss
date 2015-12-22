ef984415c6e11a4ec984eb257c64b91e8c71d676
Franziskus Kiefer <franziskuskiefer@gmail.com>
2015-12-22 14:17:49 -0800

    Bug 1221835: Avoid shift overflow. r=wtc

--- lib/freebl/mpi/mpi.c
+++ lib/freebl/mpi/mpi.c
@@ -2959,8 +2959,12 @@ mp_err   s_mp_mul_2d(mp_int *mp, mp_digit d)
   dshift = d / MP_DIGIT_BIT;
   bshift = d % MP_DIGIT_BIT;
   /* bits to be shifted out of the top word */
-  mask   = ((mp_digit)~0 << (MP_DIGIT_BIT - bshift)); 
-  mask  &= MP_DIGIT(mp, MP_USED(mp) - 1);
+  if (bshift) {
+    mask = (mp_digit)~0 << (MP_DIGIT_BIT - bshift);
+    mask &= MP_DIGIT(mp, MP_USED(mp) - 1);
+  } else {
+    mask = 0;
+  }
 
   if (MP_OKAY != (res = s_mp_pad(mp, MP_USED(mp) + dshift + (mask != 0) )))
     return res;
