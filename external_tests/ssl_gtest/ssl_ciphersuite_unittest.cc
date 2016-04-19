55c9ad482114480ebfb5dc617dd800915bcca0b8
Martin Thomson <martin.thomson@gmail.com>
2016-04-19 10:58:49 +1000

    Bug 1251136 - Enable ChaCha20Poly1305 in TLS 1.3, r=ekr,ttaubert
    --HG--
    extra : rebase_source : 09012748ce641b5b3a45305db4f5db2f1366102d
    extra : amend_source : 1fd97dde6a80972a192d600509736db6bb52315f

--- external_tests/ssl_gtest/ssl_ciphersuite_unittest.cc
+++ external_tests/ssl_gtest/ssl_ciphersuite_unittest.cc
@@ -101,17 +101,17 @@ INSTANTIATE_CIPHER_TEST_P(RC4, Stream, V10ToV12,
                           TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
                           // TLS_ECDH_RSA_WITH_RC4_128_SHA,
                           TLS_ECDHE_RSA_WITH_RC4_128_SHA);
-// TODO - Bug 1251136 move Chacha20poly1305 to V12Plus
+// TODO - Bug 1251136 move DHE_RSA suites to V12Plus
 INSTANTIATE_CIPHER_TEST_P(AEAD12, All, V12,
                           TLS_RSA_WITH_AES_128_GCM_SHA256,
                           TLS_DHE_DSS_WITH_AES_128_GCM_SHA256,
-                          TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-                          TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
                           TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
                           TLS_DHE_RSA_WITH_AES_128_GCM_SHA256);
 INSTANTIATE_CIPHER_TEST_P(AEAD, All, V12Plus,
                           TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                          TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256);
+                          TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                          TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                          TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256);
 INSTANTIATE_CIPHER_TEST_P(CBC12, All, V12,
                           TLS_DHE_RSA_WITH_AES_256_CBC_SHA256,
                           TLS_RSA_WITH_AES_256_CBC_SHA256,
