75b938b387815ff0759827f92e22675e80c1539f
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-04-28 09:38:09 +0200

    check clang-format

---
+++ automation/travis/subdir-clang-format.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if test -z $1
+then
+        echo "usage: $0 <Directory to reformat with clang-format>"
+        exit
+fi
+
+export ORIG=".orig"
+for file in `(find $1 -type f -name "*.c" && find $1 -type f -name "*.h")`;
+do
+  echo $file
+  mv "$file" "$file$ORIG"
+  clang-format "$file$ORIG" > "$file"
+done
