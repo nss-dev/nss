ab7693ea701e75b640ef885fd82f18c612f3dec0
Tim Taubert <ttaubert@mozilla.com>
2016-05-25 09:52:37 +0200

    Bug 1274350 - Make run_clang_format.sh verbose and try fixing platforms in TreeHerder r=me

--- automation/taskcluster/scripts/run_clang_format.sh
+++ automation/taskcluster/scripts/run_clang_format.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#!/usr/bin/env bash
+
+set -v -e -x
+
+if [ $(id -u) = 0 ]; then
+    # Drop privileges by re-running this script.
+    exec su worker $0 $@
+fi
 
 # Apply clang-format 3.8 on the provided folder and verify that this doesn't change any file.
 # If any file differs after formatting, the script eventually exits with 1.
@@ -10,9 +13,9 @@
 
 STATUS=0
 for i in $(find $1 -type f -name '*.[ch]' -print); do
-  if ! clang-format-3.8 $i | diff $i -; then
-    echo "Sorry, $i is not formatted properly. Please use clang-format 3.8 on your patch before landing."
-    STATUS=1
-  fi
+    if ! clang-format-3.8 $i | diff $i -; then
+        echo "Sorry, $i is not formatted properly. Please use clang-format 3.8 on your patch before landing."
+        STATUS=1
+    fi
 done
 exit $STATUS
