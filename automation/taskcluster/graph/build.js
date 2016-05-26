9fb597fd6ab7c22d1085b4f5fbf8779448afc9d4
Tim Taubert <ttaubert@mozilla.com>
2016-05-26 08:54:11 +0200

    Bug 1274350 - Add builds with NO_PKCS11_BYPASS=1 r=me

--- automation/taskcluster/graph/build.js
+++ automation/taskcluster/graph/build.js
@@ -25,6 +25,7 @@ function from_now(hours) {
 function build_task(id, def) {
   var task, retvals = [{
     taskId: taskid(id),
+    reruns: 3,
     task: task = {
       scopes: [
         "queue:route:tc-treeherder-stage.nss." + process.env.TC_REVISION,
