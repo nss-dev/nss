f088a905332f2a9e84d2e9e27bbbdac1512bc650
Mike Hommey <mh@glandium.org>
2016-07-29 20:30:56 +0900

    Bug 1290334 - Force-set CPU_ARCH to x386 or x86_64 depending on USE_64. r=wtc
    Let's put things as they are: the NSS build system is a mess. It is not
    consistent across platforms, and sometimes relies on user input,
    sometimes not.
    
    On Windows x86/x86_64, the USE_64 variable essentially chooses between
    actual x86 and x86_64. Except there are CPU_ARCH based tests in
    cross-platform Makefiles, and CPU_ARCH doesn't necessarily match what
    USE_64 says is wanted (not even mentioning what the compiler actually
    will generate). For instance, building a 32-bits NSS under MINGW64 will
    say CPU_ARCH is x86_64.
    
    This change makes CPU_ARCH match the reality imposed by USE_64 on
    Windows.

--- coreconf/WIN32.mk
+++ coreconf/WIN32.mk
@@ -219,6 +219,7 @@ ifdef USE_64
 	ifeq ($(_MSC_VER_GE_11),1)
 		LDFLAGS += -SUBSYSTEM:CONSOLE,5.02
 	endif
+	CPU_ARCH = x86_64
 else
 	DEFINES += -D_X86_
 	# VS2012 defaults to -arch:SSE2. Use -arch:IA32 to avoid requiring
@@ -231,6 +232,7 @@ else
 		endif
 		LDFLAGS += -SUBSYSTEM:CONSOLE,5.01
 	endif
+	CPU_ARCH = x386
 endif
 endif
 ifeq ($(CPU_ARCH), ALPHA)
