8ce486faa7024ff9afbddf2d503cb54ff31f5e3f
Daiki Ueno <dueno@redhat.com>
2016-08-09 19:14:27 +0200

    Bug 1293944 - Make EC test programs only depend on softoken libs, r=franziskus
    --HG--
    extra : amend_source : 0a9e0a0b9f793a069ad5ab818e8483b7b99e3136

--- cmd/ecperf/ecperf.c
+++ cmd/ecperf/ecperf.c
@@ -5,8 +5,8 @@
 #include "blapi.h"
 #include "ec.h"
 #include "ecl-curve.h"
-#include "nss.h"
-#include "secutil.h"
+#include "prprf.h"
+#include "basicutil.h"
 #include "pkcs11.h"
 #include "nspr.h"
 #include <stdio.h>
@@ -91,6 +91,8 @@ static SECOidTag ecCurve_oid_map[] = {
 
 typedef SECStatus (*op_func)(void *, void *, void *);
 typedef SECStatus (*pk11_op_func)(CK_SESSION_HANDLE, void *, void *, void *);
+
+typedef SECItem SECKEYECParams;
 
 typedef struct ThreadDataStr {
     op_func op;
@@ -710,9 +712,16 @@ main(int argv, char **argc)
         usefreebl = 1;
     }
 
-    rv = NSS_NoDB_Init(NULL);
+    rv = RNG_RNGInit();
     if (rv != SECSuccess) {
-        SECU_PrintError("Error:", "NSS_NoDB_Init");
+        SECU_PrintError("Error:", "RNG_RNGInit");
+        return -1;
+    }
+    RNG_SystemInfoForRNG();
+
+    rv = SECOID_Init();
+    if (rv != SECSuccess) {
+        SECU_PrintError("Error:", "SECOID_Init");
         goto cleanup;
     }
 
@@ -765,7 +774,8 @@ main(int argv, char **argc)
 #endif
 
 cleanup:
-    rv |= NSS_Shutdown();
+    rv |= SECOID_Shutdown();
+    RNG_RNGShutdown();
 
     if (rv != SECSuccess) {
         printf("Error: exiting with error value\n");
