7861d474930aa9b300dda713475338cd91b12248
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-08-11 10:38:35 +0200

    Bug 1294377 - add softoken clang-format to tc, r=ttaubert

--- automation/taskcluster/scripts/run_clang_format.sh
+++ automation/taskcluster/scripts/run_clang_format.sh
@@ -2,20 +2,34 @@
 
 set -v -e -x
 
-if [ $(id -u) = 0 ]; then
+if [ $(id -u) -eq 0 ]; then
     # Drop privileges by re-running this script.
-    exec su worker $0 $@
+    exec su worker $0 "$@"
 fi
 
 # Apply clang-format 3.8 on the provided folder and verify that this doesn't change any file.
 # If any file differs after formatting, the script eventually exits with 1.
 # Any differences between formatted and unformatted files is printed to stdout to give a hint what's wrong.
 
+# Includes a default set of directories.
+
+if [ $# -gt 0 ]; then
+    dirs=("$@")
+else
+    top=$(dirname $0)/../../..
+    dirs=( \
+         "$top/lib/ssl" \
+         "$top/lib/softoken" \
+    )
+fi
+
 STATUS=0
-for i in $(find $1 -type f -name '*.[ch]' -print); do
-    if ! clang-format $i | diff -Naur $i -; then
-        echo "Sorry, $i is not formatted properly. Please use clang-format 3.8 on your patch before landing."
-        STATUS=1
-    fi
+for dir in "${dirs[@]}"; do
+    for i in $(find "$dir" -type f \( -name '*.[ch]' -o -name '*.cc' \) -print); do
+        if ! clang-format "$i" | diff -Naur "$i" -; then
+            echo "Sorry, $i is not formatted properly. Please use clang-format 3.8 on your patch before landing."
+            STATUS=1
+        fi
+    done
 done
 exit $STATUS
