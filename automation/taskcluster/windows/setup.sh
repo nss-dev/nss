9a0e813cfbd1c5637c39eb1d19a34ae13bb4dabc
Martin Thomson <martin.thomson@gmail.com>
2016-08-16 11:41:36 +1000

    Bug 1294604 - Make checkout more robust, r=franziskus
    --HG--
    extra : rebase_source : 2a920f1ae9f4ac4fe6bf80077c44fa2077560ee6
    extra : amend_source : 351e756876bb86035f7d0d57214b0512d08275ee

--- automation/taskcluster/windows/setup.sh
+++ automation/taskcluster/windows/setup.sh
@@ -2,7 +2,20 @@
 
 set -v -e -x
 
-hg clone https://hg.mozilla.org/build/tools
+# Usage: hg_clone repo dir [revision=@]
+hg_clone() {
+    repo=$1
+    dir=$2
+    rev=${3:-@}
+    for i in 0 2 5; do
+        sleep $i
+        hg clone -r "$rev" "$repo" "$dir" && return
+        rm -rf "$dir"
+    done
+    exit 1
+}
+
+hg_clone https://hg.mozilla.org/build/tools tools default
 
 tools/scripts/tooltool/tooltool_wrapper.sh $(dirname $0)/releng.manifest https://api.pub.build.mozilla.org/tooltool/ non-existant-file.sh /c/mozilla-build/python/python.exe /c/builds/tooltool.py --authentication-file /c/builds/relengapi.tok -c /c/builds/tooltool_cache
 VSPATH="$(pwd)/vs2015u2"
