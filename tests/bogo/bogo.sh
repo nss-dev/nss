08ca13eea08a66e1714ddfe180aceedb9bc49df7
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-10-19 08:06:07 +0200

    Bug 1298110 - update bogo, r=me

--- tests/bogo/bogo.sh
+++ tests/bogo/bogo.sh
@@ -25,7 +25,7 @@ bogo_init()
   BORING=${BORING:=boringssl}
   if [ ! -d "$BORING" ]; then
     git clone -q https://boringssl.googlesource.com/boringssl "$BORING"
-    git -C "$BORING" checkout -q 6847f5cb9db84ad9ac4a3cf82b634e800d24e64e
+    git -C "$BORING" checkout -q 3ef7697ed30f28367395a5aafb57a12a19906d96
   fi
 
   SCRIPTNAME="bogo.sh"
