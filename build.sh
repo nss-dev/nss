853ff704fbac54c0abfa6e6a48ac389be20ae09e
Martin Thomson <martin.thomson@gmail.com>
2016-10-21 13:22:12 +1100

    Bug 1309505 - Add warning flags to gyp build, r=franziskus
    --HG--
    extra : rebase_source : 385432bb1b10fdabc168d2c8b395e51569f5a54b
    extra : amend_source : 1141abc219ebe425792155d4ee3b35386fcfe83c

--- build.sh
+++ build.sh
@@ -6,19 +6,29 @@
 #
 # -c = clean before build
 # -g = force a rebuild of gyp (and NSPR, because why not)
+# -v = verbose build
 
 set -e
 
 CWD=$(cd $(dirname $0); pwd -P)
-OBJ_DIR=$(make platform)
+OBJ_DIR=$(cd "$CWD";make platform)
 DIST_DIR="$CWD/../dist/$OBJ_DIR"
 
 if [ -n "$CCC" ] && [ -z "$CXX" ]; then
     export CXX="$CCC"
 fi
 
+while [ $# -gt 0 ]; do
+    case $1 in
+        -c) CLEAN=1 ;;
+        -g) REBUILD_GYP=1 ;;
+        -v) VERBOSE=1 ;;
+    esac
+    shift
+done
+
 # -c = clean first
-if [ "$1" = "-c" ]; then
+if [ "$CLEAN" = 1 ]; then
     rm -rf "$CWD/out"
 fi
 
@@ -27,20 +37,22 @@ if [ "$BUILD_OPT" = "1" ]; then
 else
     TARGET=Debug
 fi
-TARGET_DIR="$CWD/out/$TARGET"
-if [ "$USE_64" != "1" ]; then
+if [ "$USE_64" == "1" ]; then
+    TARGET="${TARGET}_x64"
+else
     GYP_PARAMS="-Dtarget_arch=ia32"
 fi
+TARGET_DIR="$CWD/out/$TARGET"
 
 # These steps can take a while, so don't overdo them.
 # Force a redo with -g.
-if [ "$1" = "-g" -o ! -d "$TARGET_DIR" ]; then
+if [ "$REBUILD_GYP" = 1 -o ! -d "$TARGET_DIR" ]; then
     # Build NSPR.
-    make NSS_GYP=1 install_nspr
+    make -C "$CWD" NSS_GYP=1 install_nspr
 
     # Run gyp.
     PKG_CONFIG_PATH="$CWD/../nspr/$OBJ_DIR/config" $SCANBUILD \
-        gyp -f ninja $GYP_PARAMS --depth=. --generator-output="." nss.gyp
+        gyp -f ninja $GYP_PARAMS --depth="$CWD" --generator-output="." "$CWD/nss.gyp"
 fi
 
 # Run ninja.
@@ -51,5 +63,8 @@ elif which ninja-build >/dev/null 2>&1; then
 else
     echo "Please install ninja" 1>&2
     exit 1
+fi
+if [ "$VERBOSE" = 1 ]; then
+    NINJA="$NINJA -v"
 fi
 $NINJA -C "$TARGET_DIR"
