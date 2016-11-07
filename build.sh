814541dd63028d35d5a114af48510c73c379405b
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-11-07 10:00:03 +0100

    Bug 1316836 - sancov, ubsan, and asan for gyp, r=ttaubert

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@
 
 set -e
 
+source $(dirname $0)/coreconf/nspr.sh
+
 # Usage info
 show_help() {
 cat << EOF
 
 Usage: ${0##*/} [-hcgv] [-j <n>] [--test] [--fuzz] [--scan-build[=output]]
-                [-m32] [--opt|-o]
+                [-m32] [--opt|-o] [--asan] [--ubsan] [--sancov[=edge|bb|func]]
 
 This script builds NSS with gyp and ninja.
 
@@ -31,6 +33,10 @@ NSS build tool options:
     --scan-build  run the build with scan-build (scan-build has to be in the path)
                   --scan-build=/out/path sets the output path for scan-build
     --opt|-o      do an opt build
+    --asan        do an asan build
+    --ubsan       do an ubsan build
+    --sancov      do sanitize coverage builds
+                  --sancov=func sets coverage to function level for example
 EOF
 }
 
@@ -43,17 +49,45 @@ build_64=0
 clean=0
 rebuild_gyp=0
 target=Debug
+params=$(echo "$* $CC $CCC" | tr " " "\n")
+
+cwd=$(cd $(dirname $0); pwd -P)
+dist_dir="$cwd/../dist"
 
 # try to guess sensible defaults
-arch=$(uname -m)
-if [ "$arch" = "x86_64" -o "$arch" = "aarch64" ]; then
+arch=$(python "$cwd/coreconf/detect_host_arch.py")
+if [ "$arch" = "x64" -o "$arch" = "aarch64" ]; then
     build_64=1
 fi
 
 gyp_params=()
 ninja_params=()
 scanbuild=()
-nspr_env=()
+
+check_sanitizer()
+{
+    # We can't run sanitizers on Mac for now.
+    if [ $(uname) != "Linux" ]; then
+        echo "
+         #### #### #### #### #### #### #### #### #### #### #### ####
+         ####   Sorry, sanitizers only work on Linux for now    ####
+         #### #### #### #### #### #### #### #### #### #### #### ####"
+        show_help
+        exit
+    fi
+}
+
+enable_fuzz()
+{
+    nspr_sanitizer asan
+    nspr_sanitizer ubsan
+    nspr_sanitizer sancov edge
+    gyp_params+=(-Duse_asan=1)
+    gyp_params+=(-Duse_ubsan=1)
+    gyp_params+=(-Duse_sancov=edge)
+    # Adding debug symbols even for opt builds.
+    nspr_opt+=(--enable-debug-symbols)
+}
 
 # parse command line arguments
 while [ $# -gt 0 ]; do
@@ -63,11 +97,15 @@ while [ $# -gt 0 ]; do
         -j) ninja_params+=(-j "$2"); shift ;;
         -v) ninja_params+=(-v) ;;
         --test) gyp_params+=(-Dtest_build=1) ;;
-        --fuzz) gyp_params+=(-Dtest_build=1 -Dfuzz=1) ;;
+        --fuzz) gyp_params+=(-Dtest_build=1 -Dfuzz=1); check_sanitizer; enable_fuzz ;;
         --scan-build) scanbuild=(scan-build) ;;
         --scan-build=?*) scanbuild=(scan-build -o "${1#*=}") ;;
-        --opt|-o) opt_build=1; nspr_env+=(BUILD_OPT=1) ;;
+        --opt|-o) opt_build=1 ;;
         -m32|--m32) build_64=0 ;;
+        --asan) gyp_params+=(-Duse_asan=1); nspr_sanitizer asan ;;
+        --ubsan) gyp_params+=(-Duse_ubsan=1); nspr_sanitizer ubsan ;;
+        --sancov) gyp_params+=(-Duse_sancov=edge); nspr_sanitizer sancov edge ;;
+        --sancov=?*) gyp_params+=(-Duse_sancov="${1#*=}"); nspr_sanitizer sancov "${1#*=}" ;;
         *) show_help; exit ;;
     esac
     shift
@@ -75,19 +113,25 @@ done
 
 if [ "$opt_build" = "1" ]; then
     target=Release
+    nspr_opt+=(--disable-debug --enable-optimize)
 else
     target=Debug
 fi
 if [ "$build_64" == "1" ]; then
     target="${target}_x64"
-    nspr_env+=(USE_64=1)
+    nspr_opt+=(--enable-64bit)
 else
     gyp_params+=(-Dtarget_arch=ia32)
+    nspr_opt+=(--enable-x32)
+fi
+
+# check if we have to rebuild gyp
+if [ "$params" != "$(cat $cwd/out/config 2>/dev/null)" -o "$rebuild_gyp" == 1 -o "$clean" == 1 ]; then
+    rebuild_gyp=1
+    rm -rf "$cwd/../nspr/$target" # force NSPR to rebuild
 fi
 
 # set paths
-cwd=$(cd $(dirname $0); pwd -P)
-dist_dir="$cwd/../dist"
 target_dir="$cwd/out/$target"
 
 # get the realpath of $dist_dir
@@ -100,14 +144,17 @@ echo $target > $dist_dir/latest
 obj_dir="$dist_dir/$target"
 gyp_params+=(-Dnss_dist_dir=$dist_dir)
 gyp_params+=(-Dnss_dist_obj_dir=$obj_dir)
+gyp_params+=(-Dnspr_lib_dir=$obj_dir/lib)
+gyp_params+=(-Dnspr_include_dir=$obj_dir/include/nspr)
 
 # -c = clean first
 if [ "$clean" = 1 ]; then
     rm -rf "$cwd/out"
     rm -rf "$cwd/../nspr/$target"
+    rm -rf "$dist_dir"
 fi
 
-# figure out the scan-build string
+# pass on CC and CCC
 if [ "${#scanbuild[@]}" -gt 0 ]; then
     if [ -n "$CC" ]; then
        scanbuild+=(--use-cc="$CC")
@@ -120,13 +167,14 @@ if [ "${#scanbuild[@]}" -gt 0 ]; then
 # These steps can take a while, so don't overdo them.
 # Force a redo with -g.
 if [ "$rebuild_gyp" = 1 -o ! -d "$target_dir" ]; then
-    # Build NSPR.
-    make "${nspr_env[@]}" -C "$cwd" NSS_GYP_PREFIX=$obj_dir install_nspr
+    build_nspr
 
     # Run gyp.
-    PKG_CONFIG_PATH="$obj_dir/lib/pkgconfig" \
-        "${scanbuild[@]}" gyp -f ninja "${gyp_params[@]}" --depth="$cwd" \
-          --generator-output="." "$cwd/nss.gyp"
+    "${scanbuild[@]}" gyp -f ninja "${gyp_params[@]}" --depth="$cwd" \
+      --generator-output="." "$cwd/nss.gyp"
+
+    # Store used parameters for next run.
+    echo "$params" > "$cwd/out/config"
 fi
 
 # Run ninja.
---
+++ coreconf/nspr.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script builds NSPR for NSS.
+#
+# This build system is still under development.  It does not yet support all
+# the features or platforms that the regular NSPR build supports.
+
+# variables
+nspr_opt=()
+nspr_cflags=
+nspr_cxxflags=
+nspr_ldflags=
+
+nspr_sanitizer()
+{
+    check_sanitizer
+    nspr_cflags="$nspr_cflags $(python $cwd/coreconf/sanitizers.py $1 $2)"
+    nspr_cxxflags="$nspr_cxxflags $(python $cwd/coreconf/sanitizers.py $1 $2)"
+    nspr_ldflags="$nspr_ldflags $(python $cwd/coreconf/sanitizers.py $1 $2)"
+}
+
+build_nspr()
+{
+    mkdir -p "$cwd/../nspr/$target"
+    cd "$cwd/../nspr/$target"
+    CFLAGS=$nspr_cflags CXXFLAGS=$nspr_cxxflags LDFLAGS=$nspr_ldflags \
+    CC=$CC CXX=$CCC ../configure "${nspr_opt[@]}" --prefix="$obj_dir"
+    make -C "$cwd/../nspr/$target"
+    make -C "$cwd/../nspr/$target" install
+}
