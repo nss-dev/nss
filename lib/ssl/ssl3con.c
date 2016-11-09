4f4dcd8f3c7d6494a6fd5c1305671713036645e3
Kai Engert <kaie@kuix.de>
2016-11-09 16:29:50 +0100

    Bug 1315936 - Memory error introduced by bug 1287271, r=martin.thomson

--- lib/ssl/ssl3con.c
+++ lib/ssl/ssl3con.c
@@ -7366,7 +7366,9 @@ ssl_ParseSignatureSchemes(const sslSocket *ss, PLArenaPool *arena,
     }
 
     if (!numSchemes) {
-        PORT_Free(schemes);
+        if (!arena) {
+            PORT_Free(schemes);
+        }
         schemes = NULL;
     }
 
