d6c7c14d4220c09a9f19e4ce8f5d69afd177aa57
Franziskus Kiefer <franziskuskiefer@gmail.com>
2016-11-17 09:40:29 +0100

    some build.sh fixes and improvements, r=me

--- build.sh
+++ build.sh
@@ -49,11 +49,11 @@ build_64=0
 clean=0
 rebuild_gyp=0
 target=Debug
-params=$(echo "$* $CC $CCC" | tr " " "\n")
 verbose=0
 
-cwd=$(cd $(dirname $0); pwd -P)
-dist_dir="$cwd/../dist"
+# parse parameters to store in config
+params=$(echo "$*" | sed -e 's/-c\|-v\|-g\|-j [0-9]*\|-h//g' -e 's/^[ \t]*//')
+params=$(echo "$params $CC $CCC" | tr " " "\n" | sed -e '/^\s*$/d')
 
 cwd=$(cd $(dirname $0); pwd -P)
 dist_dir="$cwd/../dist"
