b16844fe94f0d59e31d031f61580f98c6c8ed758
Tim Taubert <ttaubert@mozilla.com>
2017-01-13 13:49:14 +0100

    Ensure we have a corpus directory before running the fuzzer to handle old branches r=me

--- automation/taskcluster/scripts/fuzz.sh
+++ automation/taskcluster/scripts/fuzz.sh
@@ -2,16 +2,20 @@
 
 source $(dirname "$0")/tools.sh
 
+type="$1"
+shift
+
 # Fetch artifact if needed.
 fetch_dist
 
 # Clone corpus.
 ./nss/fuzz/clone_corpus.sh
 
+# Ensure we have a directory.
+mkdir -p nss/fuzz/corpus/$type
+
 # Fetch objdir name.
 objdir=$(cat dist/latest)
 
 # Run nssfuzz.
-type="$1"
-shift
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:dist/$objdir/lib dist/$objdir/bin/nssfuzz-"$type" "$@"
