bca5946d250fb906c2028d5fde18d37a70a63171
Martin Thomson <martin.thomson@gmail.com>
2017-01-30 11:13:05 +1100

    Bug 1333937 - False start causes SSL_Forcehandshake to report success early, a=bustage

--- gtests/ssl_gtest/tls_agent.cc
+++ gtests/ssl_gtest/tls_agent.cc
@@ -735,8 +735,10 @@ void TlsAgent::Handshake() {
   LOGV("Handshake");
   SECStatus rv = SSL_ForceHandshake(ssl_fd_);
   if (rv == SECSuccess) {
-    EXPECT_EQ(STATE_CONNECTED, state_)
-        << "the handshake callback should have been called already";
+    if (!falsestart_enabled_) {
+      EXPECT_EQ(STATE_CONNECTED, state_)
+          << "the handshake callback should have been called already";
+    }
 
     Poller::Instance()->Wait(READABLE_EVENT, adapter_, this,
                              &TlsAgent::ReadableCallback);
