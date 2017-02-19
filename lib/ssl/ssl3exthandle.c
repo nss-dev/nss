bb0fae70ae00940a9fae2f26a1e7f4ce00ab670e
Martin Thomson <martin.thomson@gmail.com>
2017-02-20 10:02:05 +1100

    Bug 1340841 - Ticket lifetime duration is in seconds, r=ekr
    --HG--
    extra : rebase_source : 31af6511eeb6e6e12c66ec5d7bcf4ad431a53386
    extra : amend_source : 0e2973620dd688efbf08c8e2a0f3ce801c4a07c9

--- lib/ssl/ssl3exthandle.c
+++ lib/ssl/ssl3exthandle.c
@@ -850,6 +850,9 @@ ssl3_ClientHandleStatusRequestXtn(const sslSocket *ss, TLSExtensionData *xtnData
     return SECSuccess;
 }
 
+PRUint32 ssl_ticket_lifetime = 2 * 24 * 60 * 60; /* 2 days in seconds */
+#define TLS_EX_SESS_TICKET_VERSION (0x0103)
+
 /*
  * Called from ssl3_SendNewSessionTicket, tls13_SendNewSessionTicket
  */
@@ -1567,8 +1570,7 @@ ssl3_ProcessSessionTicketCommon(sslSocket *ss, SECItem *data)
      * memory since the ticket is of no use.
      */
     if (parsed_session_ticket->timestamp != 0 &&
-        parsed_session_ticket->timestamp +
-                TLS_EX_SESS_TICKET_LIFETIME_HINT >
+        parsed_session_ticket->timestamp + ssl_ticket_lifetime >
             ssl_Time()) {
 
         sid = ssl3_NewSessionID(ss, PR_TRUE);
