0b9a4084a7b7222082c969402f9778a00926d01c
Martin Thomson <martin.thomson@gmail.com>
2017-03-23 14:08:36 +1100

    Bug 1348720 - Move TlsAlertRecorder to some specific alert tests, r=ttaubert

--- gtests/ssl_gtest/ssl_skip_unittest.cc
+++ gtests/ssl_gtest/ssl_skip_unittest.cc
@@ -87,12 +87,8 @@ class TlsSkipTest
 
   void ServerSkipTest(std::shared_ptr<PacketFilter> filter,
                       uint8_t alert = kTlsAlertUnexpectedMessage) {
-    auto alert_recorder = std::make_shared<TlsAlertRecorder>();
-    client_->SetPacketFilter(alert_recorder);
     server_->SetPacketFilter(filter);
     ConnectExpectAlert(client_, alert);
-    EXPECT_EQ(kTlsAlertFatal, alert_recorder->level());
-    EXPECT_EQ(alert, alert_recorder->description());
   }
 };
 
@@ -130,6 +126,8 @@ class Tls13SkipTest : public TlsConnectTestBase,
 
     server_->CheckErrorCode(error);
     ASSERT_EQ(TlsAgent::STATE_CONNECTED, client_->state());
+
+    client_->Handshake();  // Make sure to consume the alert the server sends.
   }
 };
 
@@ -218,7 +216,6 @@ TEST_P(Tls13SkipTest, SkipClientCertificate) {
   ClientSkipTest(
       std::make_shared<TlsHandshakeSkipFilter>(kTlsHandshakeCertificate),
       SSL_ERROR_RX_UNEXPECTED_CERT_VERIFY);
-  client_->Handshake();  // Make sure to consume the alert.
 }
 
 TEST_P(Tls13SkipTest, SkipClientCertificateVerify) {
@@ -228,7 +225,6 @@ TEST_P(Tls13SkipTest, SkipClientCertificateVerify) {
   ClientSkipTest(
       std::make_shared<TlsHandshakeSkipFilter>(kTlsHandshakeCertificateVerify),
       SSL_ERROR_RX_UNEXPECTED_FINISHED);
-  client_->Handshake();  // Make sure to consume the alert.
 }
 
 INSTANTIATE_TEST_CASE_P(SkipTls10, TlsSkipTest,
