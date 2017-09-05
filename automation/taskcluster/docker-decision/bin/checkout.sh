6f23633d0eb6c9a4281ffed3b311356805fbcaa9
Tim Taubert <ttaubert@mozilla.com>
2017-09-05 15:36:42 +0200

    Bug 1396771 - Move nss-decision from ttaubert/* to the nssdev/* org on DockerHub r=franziskus
    Bug #: 1396771
    
    Differential Revision: https://phabricator.services.mozilla.com/D31

--- automation/taskcluster/docker-decision/bin/checkout.sh
+++ automation/taskcluster/docker-decision/bin/checkout.sh
@@ -2,11 +2,6 @@
 
 set -v -e -x
 
-if [ $(id -u) = 0 ]; then
-    # Drop privileges by re-running this script.
-    exec su worker $0
-fi
-
 # Default values for testing.
 REVISION=${NSS_HEAD_REVISION:-default}
 REPOSITORY=${NSS_HEAD_REPOSITORY:-https://hg.mozilla.org/projects/nss}
