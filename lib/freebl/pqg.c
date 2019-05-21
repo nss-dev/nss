d893cf50f3660c89d4af8653b9b0dd870b4dcfe3
Andi-Bogdan Postelnicu <bpostelnicu@mozilla.com>
2019-05-21 17:16:13 +0000

    Bug 1227096 - assert if outlen is greater than 0. r=kaie
    Differential Revision: https://phabricator.services.mozilla.com/D31757

--- lib/freebl/pqg.c
+++ lib/freebl/pqg.c
@@ -1770,6 +1770,7 @@ PQG_VerifyParams(const PQGParams *params,
         /* 10. P generated from (L, counter, g, SEED, Q) matches P
          * in PQGParams. */
         outlen = HASH_ResultLen(hashtype) * PR_BITS_PER_BYTE;
+        PORT_Assert(outlen > 0);
         n = (L - 1) / outlen;
         offset = vfy->counter * (n + 1) + ((type == FIPS186_1_TYPE) ? 2 : 1);
         CHECK_SEC_OK(makePfromQandSeed(hashtype, L, N, offset, g, &vfy->seed,
