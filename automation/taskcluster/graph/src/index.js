d86de4a3cc10cf5fb94aa3cebf55910fcee01209
J.C. Jones <jjones@mozilla.com>
2019-06-05 14:51:12 -0700

    Bug 1555621 - import queue in the decision task r=bustage
    My try push had this correct [0], but the rebase/merge must have lost it.
    
    [0] https://hg.mozilla.org/projects/nss-try/rev/00ed423eb1810bf6c3a600ed678cd97a6c68aa35#l2.9

--- automation/taskcluster/graph/src/index.js
+++ automation/taskcluster/graph/src/index.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as try_syntax from "./try_syntax";
+import * as queue from "./queue";
 import extend from "./extend";
 
 const main = async () => {
