be90e2bc236786605cae7f9bb711d39f07f4cac6
Giulio Benetti <giulio.benetti@benettiengineering.com>
2019-10-30 08:23:07 -0700

    Bug 1590676 - Fix build if arm doesn't support NEON r=kjacobs
    At the moment NSS assumes that ARM supports NEON extension but this is
    not true and leads to build failure on ARM without NEON extension.
    Add check to assure USE_HW_AES is not defined if ARM without NEON
    extension is used.
    
    --HG--
    extra : rebase_source : fc4d7e580043c69b1d362d0ee047185dbe1539d8

--- lib/freebl/aes-armv8.c
+++ lib/freebl/aes-armv8.c
@@ -5,9 +5,10 @@
 #include "secerr.h"
 #include "rijndael.h"
 
-#if (defined(__clang__) ||                            \
-     (defined(__GNUC__) && defined(__GNUC_MINOR__) && \
-      (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8))))
+#if ((defined(__clang__) ||                                         \
+      (defined(__GNUC__) && defined(__GNUC_MINOR__) &&              \
+       (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 8)))) && \
+     (defined(__ARM_NEON) || defined(__ARM_NEON__)))
 
 #ifndef __ARM_FEATURE_CRYPTO
 #error "Compiler option is invalid"
