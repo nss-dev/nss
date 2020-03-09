7a8731e9b9743ea1d353bff2e511937c62af7723
Benjamin Beurdouche <bbeurdouche@mozilla.com>
2020-03-09 14:13:50 +0000

    Bug 1612493 - Fix Firefox build for Windows 2012 x64. r=kjacobs
    Differential Revision: https://phabricator.services.mozilla.com/D65945
    
    --HG--
    extra : moz-landing-system : lando

--- lib/freebl/verified/Hacl_Poly1305_128.c
+++ lib/freebl/verified/Hacl_Poly1305_128.c
@@ -23,7 +23,7 @@
 
 #include "Hacl_Poly1305_128.h"
 
-inline void
+void
 Hacl_Impl_Poly1305_Field32xN_128_load_acc2(Lib_IntVector_Intrinsics_vec128 *acc, uint8_t *b)
 {
     Lib_IntVector_Intrinsics_vec128 e[5U];
@@ -108,7 +108,7 @@ Hacl_Impl_Poly1305_Field32xN_128_load_acc2(Lib_IntVector_Intrinsics_vec128 *acc,
     acc[4U] = acc41;
 }
 
-inline void
+void
 Hacl_Impl_Poly1305_Field32xN_128_fmul_r2_normalize(
     Lib_IntVector_Intrinsics_vec128 *out,
     Lib_IntVector_Intrinsics_vec128 *p)
