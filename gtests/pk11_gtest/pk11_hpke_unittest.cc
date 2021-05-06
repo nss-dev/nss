70e79341b583ff45a58bf3832d76b6c8782a969b
Martin Thomson <mt@lowentropy.net>
2021-05-06 07:33:59 +0000

    Bug 1709750 - Disable HPKE test when fuzzing, r=bbeurdouche
    Differential Revision: https://phabricator.services.mozilla.com/D114443
    
    --HG--
    extra : moz-landing-system : lando

--- gtests/pk11_gtest/pk11_hpke_unittest.cc
+++ gtests/pk11_gtest/pk11_hpke_unittest.cc
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef UNSAFE_FUZZER_MODE // See Bug 1709750
+
 #include <memory>
 #include "blapi.h"
 #include "gtest/gtest.h"
@@ -683,3 +685,5 @@ TEST_F(ModeParameterizedTest, InvalidReceiverKeyType) {
 }
 
 }  // namespace nss_test
+
+#endif
