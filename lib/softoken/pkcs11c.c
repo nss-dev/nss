c9bbef53eaa39ff71be3e0a03cc51b75b8abcb1d
Aashish Arora <arora.aashish@rocketmail.com>
2021-10-22 02:05:12 -0500

    Handling CK_NSS_DB for EC key unwraping

--- lib/softoken/pkcs11c.c
+++ lib/softoken/pkcs11c.c
@@ -6293,9 +6293,9 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
             break;
         case NSSLOWKEYDSAKey:
             keyType = CKK_DSA;
-            crv = (sftk_hasAttribute(key, CKA_NSS_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
-            if (crv != CKR_OK)
-                break;
+            if (sftk_hasAttribute(key, CKA_NSS_DB)) {
+                sftk_DeleteAttributeType(key, CKA_NSS_DB);
+            }
             crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType,
                                         sizeof(keyType));
             if (crv != CKR_OK)
@@ -6335,9 +6335,9 @@ sftk_unwrapPrivateKey(SFTKObject *key, SECItem *bpki)
         /* what about fortezza??? */
         case NSSLOWKEYECKey:
             keyType = CKK_EC;
-            crv = (sftk_hasAttribute(key, CKA_NSS_DB)) ? CKR_OK : CKR_KEY_TYPE_INCONSISTENT;
-            if (crv != CKR_OK)
-                break;
+            if (sftk_hasAttribute(key, CKA_NSS_DB)) {
+                sftk_DeleteAttributeType(key, CKA_NSS_DB);
+            }
             crv = sftk_AddAttributeType(key, CKA_KEY_TYPE, &keyType,
                                         sizeof(keyType));
             if (crv != CKR_OK)
