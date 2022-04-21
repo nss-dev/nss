f0958ece44543a2e51f617c59b4136d61a4cd880
Dennis Jackson <djackson@mozilla.com>
2022-04-21 11:09:38 +0000

    Bug 1763120 - Add ECH Grease Support to tstclnt r=nss-reviewers,jschanck
    Differential Revision: https://phabricator.services.mozilla.com/D142942

--- lib/ssl/sslsock.c
+++ lib/ssl/sslsock.c
@@ -4413,7 +4413,6 @@ SSLExp_SetTls13GreaseEchSize(PRFileDesc *fd, PRUint8 size)
 {
     sslSocket *ss = ssl_FindSocket(fd);
     if (!ss || size == 0) {
-        exit(-1);
         return SECFailure;
     }
     ssl_Get1stHandshakeLock(ss);
