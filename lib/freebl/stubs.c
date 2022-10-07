f3f00cc75eae818333d329cbd9bbfc79adebc326
Anna Weine <anna.weine@mozilla.com>
2022-10-07 11:25:24 +0000

    Bug 1792821 - Updating the clang-format version to 10. r=jschanck
    Differential Revision: https://phabricator.services.mozilla.com/D158323

--- lib/freebl/stubs.c
+++ lib/freebl/stubs.c
@@ -142,11 +142,8 @@ STUB_DECLARE(int, PORT_GetError_Util, (void));
 STUB_DECLARE(PLArenaPool *, PORT_NewArena_Util, (unsigned long chunksize));
 STUB_DECLARE(void, PORT_SetError_Util, (int value));
 STUB_DECLARE(void *, PORT_ZAlloc_Util, (size_t len));
-STUB_DECLARE(void *, PORT_ZAllocAligned_Util, (size_t bytes, size_t alignment,
-                                               void **mem));
-STUB_DECLARE(void *, PORT_ZAllocAlignedOffset_Util, (size_t bytes,
-                                                     size_t alignment,
-                                                     size_t offset));
+STUB_DECLARE(void *, PORT_ZAllocAligned_Util, (size_t bytes, size_t alignment, void **mem));
+STUB_DECLARE(void *, PORT_ZAllocAlignedOffset_Util, (size_t bytes, size_t alignment, size_t offset));
 STUB_DECLARE(void, PORT_ZFree_Util, (void *ptr, size_t len));
 
 STUB_DECLARE(void, PR_Assert, (const char *s, const char *file, PRIntn ln));
@@ -156,31 +153,24 @@ STUB_DECLARE(PRStatus, PR_Close, (PRFileDesc * fd));
 STUB_DECLARE(void, PR_DestroyLock, (PRLock * lock));
 STUB_DECLARE(void, PR_DestroyCondVar, (PRCondVar * cvar));
 STUB_DECLARE(void, PR_Free, (void *ptr));
-STUB_DECLARE(char *, PR_GetLibraryFilePathname, (const char *name,
-                                                 PRFuncPtr addr));
+STUB_DECLARE(char *, PR_GetLibraryFilePathname, (const char *name, PRFuncPtr addr));
 STUB_DECLARE(PRFileDesc *, PR_ImportPipe, (PROsfd osfd));
 STUB_DECLARE(void, PR_Lock, (PRLock * lock));
 STUB_DECLARE(PRCondVar *, PR_NewCondVar, (PRLock * lock));
 STUB_DECLARE(PRLock *, PR_NewLock, (void));
 STUB_DECLARE(PRStatus, PR_NotifyCondVar, (PRCondVar * cvar));
 STUB_DECLARE(PRStatus, PR_NotifyAllCondVar, (PRCondVar * cvar));
-STUB_DECLARE(PRFileDesc *, PR_Open, (const char *name, PRIntn flags,
-                                     PRIntn mode));
+STUB_DECLARE(PRFileDesc *, PR_Open, (const char *name, PRIntn flags, PRIntn mode));
 STUB_DECLARE(PRInt32, PR_Read, (PRFileDesc * fd, void *buf, PRInt32 amount));
-STUB_DECLARE(PROffset32, PR_Seek, (PRFileDesc * fd, PROffset32 offset,
-                                   PRSeekWhence whence));
+STUB_DECLARE(PROffset32, PR_Seek, (PRFileDesc * fd, PROffset32 offset, PRSeekWhence whence));
 STUB_DECLARE(PRStatus, PR_Sleep, (PRIntervalTime ticks));
 STUB_DECLARE(PRStatus, PR_Unlock, (PRLock * lock));
-STUB_DECLARE(PRStatus, PR_WaitCondVar, (PRCondVar * cvar,
-                                        PRIntervalTime timeout));
+STUB_DECLARE(PRStatus, PR_WaitCondVar, (PRCondVar * cvar, PRIntervalTime timeout));
 STUB_DECLARE(char *, PR_GetEnvSecure, (const char *));
 
-STUB_DECLARE(SECItem *, SECITEM_AllocItem_Util, (PLArenaPool * arena,
-                                                 SECItem *item, unsigned int len));
-STUB_DECLARE(SECComparison, SECITEM_CompareItem_Util, (const SECItem *a,
-                                                       const SECItem *b));
-STUB_DECLARE(SECStatus, SECITEM_CopyItem_Util, (PLArenaPool * arena,
-                                                SECItem *to, const SECItem *from));
+STUB_DECLARE(SECItem *, SECITEM_AllocItem_Util, (PLArenaPool * arena, SECItem *item, unsigned int len));
+STUB_DECLARE(SECComparison, SECITEM_CompareItem_Util, (const SECItem *a, const SECItem *b));
+STUB_DECLARE(SECStatus, SECITEM_CopyItem_Util, (PLArenaPool * arena, SECItem *to, const SECItem *from));
 STUB_DECLARE(void, SECITEM_FreeItem_Util, (SECItem * zap, PRBool freeit));
 STUB_DECLARE(void, SECITEM_ZfreeItem_Util, (SECItem * zap, PRBool freeit));
 STUB_DECLARE(SECOidTag, SECOID_FindOIDTag_Util, (const SECItem *oid));
