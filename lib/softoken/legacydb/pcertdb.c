c02aed5ac68f7a1bc6d38b6654c769ebc7d4905f
Robert Relyea <rrelyea@redhat.com>
2023-05-15 09:33:44 -0700

    Back out pcertdb patch attached to wrong branch.
    --HG--
    branch : NSS_3_89_BRANCH

--- lib/softoken/legacydb/pcertdb.c
+++ lib/softoken/legacydb/pcertdb.c
@@ -4277,7 +4277,6 @@ CreateTrust(void)
     if (trust) {
         trustListCount--;
         trustListHead = trust->next;
-        trust->next = NULL;
     }
     PORT_Assert(trustListCount >= 0);
     nsslowcert_UnlockFreeList();
@@ -5161,11 +5160,9 @@ nsslowcert_hasTrust(NSSLOWCERTCertTrust *trust)
     if (trust == NULL) {
         return PR_FALSE;
     }
-    /* if we only have CERTDB__USER and CERTDB_TRUSTED_UNKNOWN bits, then
-     * we don't have a trust record. */
-    return !(((trust->sslFlags & ~(CERTDB_USER | CERTDB_TRUSTED_UNKNOWN)) == 0) &&
-             ((trust->emailFlags & ~(CERTDB_USER | CERTDB_TRUSTED_UNKNOWN)) == 0) &&
-             ((trust->objectSigningFlags & ~(CERTDB_USER | CERTDB_TRUSTED_UNKNOWN)) == 0));
+    return !((trust->sslFlags & CERTDB_TRUSTED_UNKNOWN) &&
+             (trust->emailFlags & CERTDB_TRUSTED_UNKNOWN) &&
+             (trust->objectSigningFlags & CERTDB_TRUSTED_UNKNOWN));
 }
 
 /*
