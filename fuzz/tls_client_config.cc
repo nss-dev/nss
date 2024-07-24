c039f30cccc75e2f1e89c38e1bc845d081f342d7
Maurice Dauer <mdauer@mozilla.com>
2024-07-24 18:15:40 +0000

    Bug 1899542: Add fuzzing support for SSL_SetCertificateCompressionAlgorithm, SSL_SetClientEchConfigs, SSL_VersionRangeSet and SSL_AddExternalPsk, r=djackson
    Differential Revision: https://phabricator.services.mozilla.com/D212424
    
    --HG--
    extra : moz-landing-system : lando

--- fuzz/tls_client_config.cc
+++ fuzz/tls_client_config.cc
@@ -2,26 +2,59 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <cstdint>
+
+#include "prtypes.h"
+
 #include "tls_client_config.h"
 
-const uint64_t CONFIG_FAIL_CERT_AUTH = 1 << 0;
-const uint64_t CONFIG_ENABLE_EXTENDED_MS = 1 << 1;
-const uint64_t CONFIG_REQUIRE_DH_NAMED_GROUPS = 1 << 2;
-const uint64_t CONFIG_ENABLE_FALSE_START = 1 << 3;
-const uint64_t CONFIG_ENABLE_DEFLATE = 1 << 4;
-const uint64_t CONFIG_ENABLE_CBC_RANDOM_IV = 1 << 5;
-const uint64_t CONFIG_REQUIRE_SAFE_NEGOTIATION = 1 << 6;
-const uint64_t CONFIG_ENABLE_CACHE = 1 << 7;
-const uint64_t CONFIG_ENABLE_GREASE = 1 << 8;
-const uint64_t CONFIG_ENABLE_CH_EXTENSION_PERMUTATION = 1 << 9;
+const uint32_t CONFIG_FAIL_CERT_AUTH = 1 << 0;
+const uint32_t CONFIG_ENABLE_EXTENDED_MS = 1 << 1;
+const uint32_t CONFIG_REQUIRE_DH_NAMED_GROUPS = 1 << 2;
+const uint32_t CONFIG_ENABLE_FALSE_START = 1 << 3;
+const uint32_t CONFIG_ENABLE_DEFLATE = 1 << 4;
+const uint32_t CONFIG_ENABLE_CBC_RANDOM_IV = 1 << 5;
+const uint32_t CONFIG_REQUIRE_SAFE_NEGOTIATION = 1 << 6;
+const uint32_t CONFIG_ENABLE_CACHE = 1 << 7;
+const uint32_t CONFIG_ENABLE_GREASE = 1 << 8;
+const uint32_t CONFIG_ENABLE_CH_EXTENSION_PERMUTATION = 1 << 9;
+const uint32_t CONFIG_SET_CERTIFICATION_COMPRESSION_ALGORITHM = 1 << 10;
+const uint32_t CONFIG_SET_CLIENT_ECH_CONFIGS = 1 << 11;
+const uint32_t CONFIG_VERSION_RANGE_SET = 1 << 12;
+const uint32_t CONFIG_ADD_EXTERNAL_PSK = 1 << 13;
 
 // XOR 64-bit chunks of data to build a bitmap of config options derived from
 // the fuzzing input. This seems the only way to fuzz various options while
 // still maintaining compatibility with BoringSSL or OpenSSL fuzzers.
 ClientConfig::ClientConfig(const uint8_t* data, size_t len) {
+  union {
+    uint64_t bitmap;
+    struct {
+      uint32_t config;
+      PRUint16 ssl_version_range_min;
+      PRUint16 ssl_version_range_max;
+    };
+  };
+
   for (size_t i = 0; i < len; i++) {
-    config_ ^= static_cast<uint64_t>(data[i]) << (8 * (i % 8));
+    bitmap ^= static_cast<uint64_t>(data[i]) << (8 * (i % 8));
   }
+
+  // Map SSL version values to a valid range.
+  ssl_version_range_min =
+      SSL_VERSION_RANGE_MIN_VALID +
+      (ssl_version_range_min %
+       (1 + SSL_VERSION_RANGE_MAX_VALID - SSL_VERSION_RANGE_MIN_VALID));
+  ssl_version_range_max =
+      ssl_version_range_min +
+      (ssl_version_range_max %
+       (1 + SSL_VERSION_RANGE_MAX_VALID - ssl_version_range_min));
+
+  config_ = config;
+  ssl_version_range_ = {
+      .min = ssl_version_range_min,
+      .max = ssl_version_range_max,
+  };
 }
 
 bool ClientConfig::FailCertificateAuthentication() {
@@ -57,3 +90,23 @@ bool ClientConfig::EnableGrease() { return config_ & CONFIG_ENABLE_GREASE; }
 bool ClientConfig::EnableCHExtensionPermutation() {
   return config_ & CONFIG_ENABLE_CH_EXTENSION_PERMUTATION;
 };
+
+bool ClientConfig::SetCertificateCompressionAlgorithm() {
+  return config_ & CONFIG_SET_CERTIFICATION_COMPRESSION_ALGORITHM;
+}
+
+bool ClientConfig::SetClientEchConfigs() {
+  return config_ & CONFIG_SET_CLIENT_ECH_CONFIGS;
+}
+
+bool ClientConfig::SetVersionRange() {
+  return config_ & CONFIG_VERSION_RANGE_SET;
+}
+
+bool ClientConfig::AddExternalPsk() {
+  return config_ & CONFIG_ADD_EXTERNAL_PSK;
+}
+
+const SSLVersionRange& ClientConfig::VersionRange() {
+  return ssl_version_range_;
+}
