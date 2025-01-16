93490d4a9bd389a7b1c99f05beff705b7433cb8e
Julien Cristau <jcristau@mozilla.com>
2025-01-16 11:02:56 +0000

    Bug 1831552 - ci: let the windows setup script work without $m r=nss-reviewers,nkulatova
    The $m variable is set by the build_gyp.sh and build.sh scripts, but not
    by gen_certs.sh and run_tests.sh, which also source setup.sh.  On the
    other hand, they don't appear to need visual studio environment
    variables.
    
    For some reason this didn't use to fail on the win2012 workers, even
    though the script uses `set -e`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D232869
    
    --HG--
    extra : moz-landing-system : lando

--- automation/taskcluster/windows/setup.sh
+++ automation/taskcluster/windows/setup.sh
@@ -23,34 +23,36 @@ tools/scripts/tooltool/tooltool_wrapper.sh \
     -c /c/builds/tooltool_cache
 
 # This needs $m to be set.
-[[ -n "$m" ]]
+if [ -n "$m" ]; then
 
-# Setup MSVC paths.
-export VSPATH="${PWD}/vs2017_15.4.2"
-UCRTVersion="10.0.15063.0"
+    # Setup MSVC paths.
+    export VSPATH="${PWD}/vs2017_15.4.2"
+    UCRTVersion="10.0.15063.0"
 
-export WINDOWSSDKDIR="${VSPATH}/SDK"
-export VS90COMNTOOLS="${VSPATH}/VC"
-export WIN32_REDIST_DIR="${VSPATH}/VC/redist/${m}/Microsoft.VC141.CRT"
-export WIN_UCRT_REDIST_DIR="${VSPATH}/SDK/Redist/ucrt/DLLs/${m}"
+    export WINDOWSSDKDIR="${VSPATH}/SDK"
+    export VS90COMNTOOLS="${VSPATH}/VC"
+    export WIN32_REDIST_DIR="${VSPATH}/VC/redist/${m}/Microsoft.VC141.CRT"
+    export WIN_UCRT_REDIST_DIR="${VSPATH}/SDK/Redist/ucrt/DLLs/${m}"
 
-if [ "$m" == "x86" ]; then
-    PATH="${PATH}:${VSPATH}/VC/bin/Hostx64/x86"
-    PATH="${PATH}:${VSPATH}/VC/bin/Hostx64/x64"
+    if [ "$m" == "x86" ]; then
+        PATH="${PATH}:${VSPATH}/VC/bin/Hostx64/x86"
+        PATH="${PATH}:${VSPATH}/VC/bin/Hostx64/x64"
+    fi
+    PATH="${PATH}:${VSPATH}/VC/bin/Host${m}/${m}"
+    PATH="${PATH}:${WIN32_REDIST_DIR}"
+    PATH="${PATH}:${WIN_UCRT_REDIST_DIR}"
+    PATH="${PATH}:${VSPATH}/SDK/bin/${UCRTVersion}/x64"
+    export PATH
+
+    LIB="${LIB}:${VSPATH}/VC/lib/${m}"
+    LIB="${LIB}:${VSPATH}/SDK/lib/${UCRTVersion}/ucrt/${m}"
+    LIB="${LIB}:${VSPATH}/SDK/lib/${UCRTVersion}/um/${m}"
+    export LIB
+
+    INCLUDE="${INCLUDE}:${VSPATH}/VC/include"
+    INCLUDE="${INCLUDE}:${VSPATH}/SDK/Include/${UCRTVersion}/ucrt"
+    INCLUDE="${INCLUDE}:${VSPATH}/SDK/Include/${UCRTVersion}/shared"
+    INCLUDE="${INCLUDE}:${VSPATH}/SDK/Include/${UCRTVersion}/um"
+    export INCLUDE
+
 fi
-PATH="${PATH}:${VSPATH}/VC/bin/Host${m}/${m}"
-PATH="${PATH}:${WIN32_REDIST_DIR}"
-PATH="${PATH}:${WIN_UCRT_REDIST_DIR}"
-PATH="${PATH}:${VSPATH}/SDK/bin/${UCRTVersion}/x64"
-export PATH
-
-LIB="${LIB}:${VSPATH}/VC/lib/${m}"
-LIB="${LIB}:${VSPATH}/SDK/lib/${UCRTVersion}/ucrt/${m}"
-LIB="${LIB}:${VSPATH}/SDK/lib/${UCRTVersion}/um/${m}"
-export LIB
-
-INCLUDE="${INCLUDE}:${VSPATH}/VC/include"
-INCLUDE="${INCLUDE}:${VSPATH}/SDK/Include/${UCRTVersion}/ucrt"
-INCLUDE="${INCLUDE}:${VSPATH}/SDK/Include/${UCRTVersion}/shared"
-INCLUDE="${INCLUDE}:${VSPATH}/SDK/Include/${UCRTVersion}/um"
-export INCLUDE
