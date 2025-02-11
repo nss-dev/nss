9663da3bc8e142dda90f07915e1b688322cd7812
Julien Cristau <jcristau@mozilla.com>
2025-02-11 14:28:37 +0000

    Bug 1854095 - parse try syntax on pushes to nss-try. r=releng-reviewers,bhearsum,bbeurdouche
    Differential Revision: https://phabricator.services.mozilla.com/D236190
    
    --HG--
    extra : moz-landing-system : lando

--- taskcluster/nss_taskgraph/transforms/platforms.py
+++ taskcluster/nss_taskgraph/transforms/platforms.py
@@ -26,6 +26,9 @@ def add_env_vars(config, tasks):
         if task["attributes"]["build_platform"].startswith("mac"):
             env.update({"NSS_TASKCLUSTER_MAC": "1"})
 
+        if config.params["try_options"].get("allow_nspr_patch"):
+            env.update({"ALLOW_NSPR_PATCH": "1"})
+
         yield task
 
 
