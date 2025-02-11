e7612921c4922399caa7cc188becb613f6822468
Julien Cristau <jcristau@mozilla.com>
2025-02-11 14:28:35 +0000

    Bug 1854095 - add "tools" task kind. r=releng-reviewers,bhearsum,bbeurdouche
    Update scripts to:
    - stop cloning the source, taken care of by run-task
    - look for repos in ${VCS_PATH}, copying to the work dir in some cases
      to avoid polluting the cache
    
    Differential Revision: https://phabricator.services.mozilla.com/D236185
    
    --HG--
    extra : moz-landing-system : lando

--- automation/taskcluster/scripts/run_hacl.sh
+++ automation/taskcluster/scripts/run_hacl.sh
@@ -8,7 +8,6 @@ fi
 
 set -e -x -v
 
-# The docker image this is running in has NSS sources.
 # Get the HACL* source, containing a snapshot of the C code, extracted on the
 # HACL CI.
 git clone -q "https://github.com/hacl-star/hacl-star" ~/hacl-star
@@ -16,17 +15,17 @@ git -C ~/hacl-star checkout -q 0f136f28935822579c244f287e1d2a1908a7e552
 
 # Format the C snapshot.
 cd ~/hacl-star/dist/mozilla
-cp ~/nss/.clang-format .
+cp ${VCS_PATH}/nss/.clang-format .
 find . -type f -name '*.[ch]' -exec clang-format -i {} \+
 cd ~/hacl-star/dist/karamel
-cp ~/nss/.clang-format .
+cp ${VCS_PATH}/nss/.clang-format .
 find . -type f -name '*.[ch]' -exec clang-format -i {} \+
 cd ~/hacl-star/dist/gcc-compatible
-cp ~/nss/.clang-format .
+cp ${VCS_PATH}/nss/.clang-format .
 find . -type f -name '*.[ch]' -exec clang-format -i {} \+
 
 cd ~/hacl-star
-patches=(~/nss/automation/taskcluster/scripts/patches/*.patch)
+patches=(${VCS_PATH}/nss/automation/taskcluster/scripts/patches/*.patch)
 for f in "${patches[@]}"; do
     git apply "$f"
 done
@@ -39,7 +38,7 @@ done
 # For instance, the files Hacl_Chacha20.h are present in both directories, but the content differs.
 
 # TODO(Bug 1899443): remove these exceptions
-files=($(find ~/nss/lib/freebl/verified/internal -type f -name '*.[ch]' -not -path "*/freebl/verified/internal/libcrux*"))
+files=($(find ${VCS_PATH}/nss/lib/freebl/verified/internal -type f -name '*.[ch]' -not -path "*/freebl/verified/internal/libcrux*"))
 for f in "${files[@]}"; do
     file_name=$(basename "$f")
     hacl_file=($(find ~/hacl-star/dist/mozilla/internal/ -type f -name $file_name))
@@ -51,7 +50,7 @@ for f in "${files[@]}"; do
     diff -u $hacl_file $f
 done
 
-files=($(find ~/nss/lib/freebl/verified/ -type f -name '*.[ch]' -not -path "*/freebl/verified/internal/*" -not -path "*/freebl/verified/config.h" -not -path "*/freebl/verified/libcrux*"))
+files=($(find ${VCS_PATH}/nss/lib/freebl/verified/ -type f -name '*.[ch]' -not -path "*/freebl/verified/internal/*" -not -path "*/freebl/verified/config.h" -not -path "*/freebl/verified/libcrux*"))
 for f in "${files[@]}"; do
     file_name=$(basename "$f")
     hacl_file=($(find ~/hacl-star/dist/mozilla/ ~/hacl-star/dist/karamel/ -type f -name $file_name -not -path "*/hacl-star/dist/mozilla/internal/*"))
@@ -76,7 +75,7 @@ done
 # Here we process the code that's not located in /hacl-star/dist/mozilla/ but
 # /hacl-star/dist/gcc-compatible. 
 
-files=($(find ~/nss/lib/freebl/verified/internal -type f -name '*.[ch]'))
+files=($(find ${VCS_PATH}/nss/lib/freebl/verified/internal -type f -name '*.[ch]'))
 for f in "${files[@]}"; do
     file_name=$(basename "$f")
     hacl_file=($(find ~/hacl-star/dist/gcc-compatible/internal/ -type f -name $file_name))
@@ -88,7 +87,7 @@ for f in "${files[@]}"; do
     diff -u $hacl_file $f
 done
 
-files=($(find ~/nss/lib/freebl/verified/ -type f -name '*.[ch]' -not -path "*/freebl/verified/internal/*"))
+files=($(find ${VCS_PATH}/nss/lib/freebl/verified/ -type f -name '*.[ch]' -not -path "*/freebl/verified/internal/*"))
 for f in "${files[@]}"; do
     file_name=$(basename "$f")
     hacl_file=($(find ~/hacl-star/dist/gcc-compatible/ -type f -name $file_name -not -path "*/hacl-star/dist/gcc-compatible/internal/*"))
