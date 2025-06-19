38d3b9a1dee45395d84fabcad0d4d3953411b2df
Anna Weine <anna.weine@mozilla.com>
2025-06-19 16:53:06 +0200

    Set version numbers to 3.114 Beta

--- lib/nss/nss.h
+++ lib/nss/nss.h
@@ -22,9 +22,9 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <ECC>][ <Beta>]"
  */
-#define NSS_VERSION "3.113" _NSS_CUSTOMIZED
+#define NSS_VERSION "3.114" _NSS_CUSTOMIZED
 #define NSS_VMAJOR 3
-#define NSS_VMINOR 113
+#define NSS_VMINOR 114
 #define NSS_VPATCH 0
 #define NSS_VBUILD 0
 #define NSS_BETA PR_FALSE
