44ac5968f67ecce6f58810c794730c97ce838b40
Benjamin Beurdouche <bbeurdouche@mozilla.com>
2025-08-15 08:22:36 +0200

    Set version numbers to 3.116 Beta

--- lib/util/nssutil.h
+++ lib/util/nssutil.h
@@ -19,9 +19,9 @@
  * The format of the version string should be
  *     "<major version>.<minor version>[.<patch level>[.<build number>]][ <Beta>]"
  */
-#define NSSUTIL_VERSION "3.115 Beta"
+#define NSSUTIL_VERSION "3.116 Beta"
 #define NSSUTIL_VMAJOR 3
-#define NSSUTIL_VMINOR 115
+#define NSSUTIL_VMINOR 116
 #define NSSUTIL_VPATCH 0
 #define NSSUTIL_VBUILD 0
 #define NSSUTIL_BETA PR_TRUE
