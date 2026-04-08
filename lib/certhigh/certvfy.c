8d77dcbfde0278446d98b3e939692ebed5f88ac8
John Schanck <jschanck@mozilla.com>
2026-04-08 03:16:19 +0000

    Bug 2029425 - Heap use-after-free in cert_VerifyCertChainOld via dangling certsList[] entry on NameConstraints violation. r=#nss-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D292673
    
    --HG--
    branch : NSS_3_90_BRANCH

--- lib/certhigh/certvfy.c
+++ lib/certhigh/certvfy.c
@@ -722,7 +722,7 @@ cert_VerifyCertChainOld(CERTCertDBHandle *handle, CERTCertificate *cert,
                 certsList = tmpCertsList;
             }
             for (i = 0; i < subjectNameListLen; i++) {
-                certsList[namesCount + i] = subjectCert;
+                certsList[namesCount + i] = CERT_DupCertificate(subjectCert);
             }
             namesCount += subjectNameListLen;
             namesList = cert_CombineNamesLists(namesList, subjectNameList);
@@ -979,6 +979,11 @@ loser:
     rv = SECFailure;
 done:
     if (certsList != NULL) {
+        for (int i = 0; i < namesCount; i++) {
+            if (certsList[i]) {
+                CERT_DestroyCertificate(certsList[i]);
+            }
+        }
         PORT_Free(certsList);
     }
     if (issuerCert) {
